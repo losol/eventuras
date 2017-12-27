254559cb3d787998716ed8650a186c213d1923e4
Ole Kristian Losvik <losvik@gmail.com>
2017-12-27 21:05:11 +0100

    seed data

---
+++ Data/SeedData.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using losol.EventManagement.Models;
+
+namespace losol.EventManagement.Data
+{
+	public class SeedData
+	{
+        public static void Initialize(ApplicationDbContext context)
+		{
+			// Look for any events.
+			if (context.EventInfos.Any())
+			{
+				return;   // DB has been seeded
+			}
+
+            var eventInfos = new EventInfo[]
+            {
+                new EventInfo{Title="Test 01", Code="Test01", Description="A test event."},
+                new EventInfo{Title="Test 02", Code="TEST02", Description="Another test event."}
+            };
+
+            foreach (var item in eventInfos)
+            {
+                context.EventInfos.AddAsync(item);
+            }
+
+            context.SaveChanges();
+
+
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using losol.EventManagement.Data;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace losol.EventManagement
@@ -14,6 +16,13 @@ namespace losol.EventManagement
     {
         public static void Main(string[] args)
         {
+            var host = BuildWebHost(args);
+            var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<ApplicationDbContext>();
+
+            SeedData.Initialize(context);
+
             BuildWebHost(args).Run();
         }
 
