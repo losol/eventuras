4f1ad2642ef521505cbd8a4f57051408f4a812a7
Ole Kristian Losvik <losvik@gmail.com>
2017-12-27 23:36:09 +0100

    fix

--- Pages/Register/Event.cshtml.cs
+++ Pages/Register/Event.cshtml.cs
@@ -8,12 +8,21 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using losol.EventManagement.Data;
 using losol.EventManagement.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using losol.EventManagement.Services;
+using losol.EventManagement.Pages.Account;
+using System.ComponentModel.DataAnnotations;
 
 namespace losol.EventManagement.Pages.Register
 {
     public class EventRegistrationModel : PageModel
     {
         private readonly losol.EventManagement.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<LoginModel> _logger;
+        private readonly IEmailSender _emailSender;
+
         public string EventTitle;
         public string EventDescription;
 
@@ -23,20 +32,45 @@ namespace losol.EventManagement.Pages.Register
         }
 
         [BindProperty]
-        public Models.Registration Registration { get; set; }
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [StringLength(100)]
+            [Display(Name = "Navn")]
+            public string Name { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "E-post")]
+            public string Email { get; set; }
+
+            [Required]
+            [Display(Name = "Mobiltelefon")]
+            public string Phone { get; set; }
+
+            [Display(Name = "Arbeidsplass")]
+            public string Employer { get; set; }
+
+            [Display(Name = "Organisasjonsnummer")]
+            public string VatNumber { get; set; }
+
+            [Display(Name = "Betalingsmetode")]
+            public PaymentMethod? PaymentMethod { get; set; }
+        }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToPage("./Index");
             }
 
             var eventinfo = await _context.EventInfos.FirstOrDefaultAsync(m => m.EventInfoId == id);
             if (eventinfo == null)
             {
-                return NotFound();
-                
+                return NotFound(); 
             }
             else 
             {
@@ -48,10 +82,13 @@ namespace losol.EventManagement.Pages.Register
         }
 
 
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(int? id)
         {
             if (!ModelState.IsValid)
             {
+                var eventinfo = await _context.EventInfos.FirstOrDefaultAsync(m => m.EventInfoId == id);
+                EventTitle = eventinfo.Title;
+                EventDescription = eventinfo.Description;
                 return Page();
             }
 
