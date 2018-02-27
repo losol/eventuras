a2135a70fa50b08319853e1f7a2bbe60ee05ef20
Ole Kristian Losvik <losvik@gmail.com>
2018-02-27 20:55:32 +0100

    added welcome letter

--- Pages/Register/Confirm.cshtml.cs
+++ Pages/Register/Confirm.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using losol.EventManagement.Data;
 using losol.EventManagement.Models;
 using losol.EventManagement.Services;
+using losol.EventManagement.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -18,17 +19,21 @@ namespace losol.EventManagement.Pages.Register
 		private readonly IEmailSender _emailSender;
 		private readonly UserManager<ApplicationUser> _userManager;
 
+		private IRenderService _renderService;
+
         public string Message { get; set; }
 
         public ConfirmModel(
 			ApplicationDbContext context,
 			IEmailSender emailSender,
-			UserManager<ApplicationUser> userManager
+			UserManager<ApplicationUser> userManager,
+			IRenderService renderService
 			)
 		{
 			_context = context;
 			_emailSender = emailSender;
 			_userManager = userManager;
+            _renderService = renderService;
 		}
 
         public Registration Registration {get;set;}
@@ -49,17 +54,40 @@ namespace losol.EventManagement.Pages.Register
 
             if (HttpContext.Request.Query["auth"] == Registration.VerificationCode) 
             {
-                Message += " success";
+                // Set and save verified registration.
                 Registration.Verified = true;
                 await  _context.SaveChangesAsync();
 
-                var user = await _userManager.FindByIdAsync(Registration.UserId);
+                // Get the user which has registered for the event.
+                var participant = await _userManager.FindByIdAsync(Registration.UserId);
+
+                // Get the event which the user has registered for.
+                var eventinfo = await _context.EventInfos.SingleOrDefaultAsync(n => n.EventInfoId == Registration.EventInfoId);
 
 
-                await _emailSender.SendEmailAsync("losvik@gmail.com",
-                "Påmelding kurs",
-                $@"{Registration.UserId}, {user.Email}  har meldt seg på kurset {Registration.EventInfoId}
-                Bare så du vet det");
+                // Send notification to admin
+                var adminEmail = new EmailMessage()
+				{
+					Name = Registration.ParticipantName,
+					Email = "losvik@gmail.com",  // TODO: Get admin email from app settings.
+					Subject = $@"Kurspåmelding {eventinfo.Title}!",
+					Message = $@"{participant.Name} ({participant.Email}) har meldt seg på kurset {eventinfo.Title} (id {Registration.EventInfoId}). 
+                        Bare så du vet det"
+				};
+                var adminEmailString = await _renderService.RenderViewToStringAsync("Templates/Email/StandardEmail", adminEmail);
+                await _emailSender.SendEmailAsync(adminEmail.Email, adminEmail.Subject, adminEmailString);
+
+                // Send welcome letter to participant
+				var participantEmail = new EmailMessage()
+				{
+					Name = Registration.ParticipantName,
+					Email = participant.Email,
+					Subject = $@"Velkommen til {eventinfo.Title}!",
+					Message = eventinfo.WelcomeLetter
+				};
+
+				var participantEmailString = await _renderService.RenderViewToStringAsync("Templates/Email/StandardEmail", participantEmail);
+				await _emailSender.SendEmailAsync(participantEmail.Email, participantEmail.Subject, participantEmailString);
                 return RedirectToPage("/Register/Confirmed");
             }
 
