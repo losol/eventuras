d2c96e2c024aecd9284ba7fceae3f807664a3a49
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-01 15:33:14 +0530

    Remove obsolete comment
    lockoutOnFailure has already been set to true.

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -68,8 +68,6 @@ namespace losol.EventManagement.Pages.Account
 
             if (ModelState.IsValid)
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: true);
                 if (result.Succeeded)
                 {
