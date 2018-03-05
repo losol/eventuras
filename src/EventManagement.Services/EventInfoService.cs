67a1326bf4880afac6db170740372968a450b434
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-05 17:23:38 +0530

    Write a basic EventInfoService

---
+++ src/EventManagement.Services/EventInfoService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using losol.EventManagement.Domain;
+using losol.EventManagement.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace losol.EventManagement.Services
+{
+	public class EventInfoService : IEventInfoService
+	{
+		private readonly ApplicationDbContext _db;
+
+		public EventInfoService(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public async Task<List<EventInfo>> GetFeaturedEventsAsync() 
+		{
+			return await _db.EventInfos.Where(i => i.Published && i.Featured)
+					  .ToListAsync();
+		}
+
+	}
+}
---
+++ src/EventManagement.Services/IEventInfoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using losol.EventManagement.Domain;
+
+namespace losol.EventManagement.Services
+{
+	public interface IEventInfoService
+	{
+		Task<List<EventInfo>> GetFeaturedEventsAsync();
+	}
+}
