4d2776640d26707bafa0978f7af234dd7cf103d2
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-07 09:17:12 +0530

    Introduce a PaymentMethodService

---
+++ src/EventManagement.Services/IPaymentMethodService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using losol.EventManagement.Domain;
+
+namespace losol.EventManagement.Services
+{
+	public interface IPaymentMethodService
+	{
+		Task<PaymentMethod> GetAsync(int id);
+		Task<List<PaymentMethod>> GetActivePaymentMethodsAsync();
+		int GetDefaultPaymentMethodId();
+		Task<PaymentMethod> GetDefaultPaymentMethod();
+	}
+}
