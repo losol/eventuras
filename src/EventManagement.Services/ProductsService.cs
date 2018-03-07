7039045bcabec411b4e9eb1dd75032d1702a35bb
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-07 16:48:20 +0530

    Create a simple ProductsService

---
+++ src/EventManagement.Services/ProductsService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using losol.EventManagement.Domain;
+using losol.EventManagement.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace losol.EventManagement.Services
+{
+	public class ProductsService : IProductsService
+	{
+		private readonly ApplicationDbContext _db;
+
+		public ProductsService(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public Task<List<Product>> GetAsync()
+		{
+			return _db.Products
+				      .Include(p => p.ProductVariants)
+				      .AsNoTracking()
+				      .ToListAsync();
+		}
+
+		public Task<List<Product>> GetForEventAsync(int eventId)
+		{
+			return _db.Products
+				      .Where(p => p.EventInfoId == eventId)
+					  .Include(p => p.ProductVariants)
+					  .AsNoTracking()
+					  .ToListAsync();
+		}
+	}
+}
