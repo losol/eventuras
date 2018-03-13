e9dea8aa71dd6394d0ff540a5dd6311a03b1caf1
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-13 12:49:35 +0530

    Add unit tests for Registration.Verify()

---
+++ tests/EventManagement.Domain.Tests/RegistrationTests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace losol.EventManagement.Domain.Tests
+{
+	public class RegistrationTests
+	{
+		public class Verify_Should
+		{
+			[Fact]
+			public void SucceedWhenNotVerified()
+			{
+				Registration registration = new Registration();
+				var expected = true;
+
+				registration.Verify();
+				var actual = registration.Verified;
+
+				Assert.Equal(expected, actual);
+			}
+
+			[Fact]
+			public void SucceedWhenAlreadyVerified()
+			{
+				Registration registration = new Registration { Verified = true };
+				var expected = true;
+
+				registration.Verify();
+				var actual = registration.Verified;
+
+				Assert.Equal(expected, actual);
+			}
+		}
+	}
+}
