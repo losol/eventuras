b2646625c1e18bc81460b71e2e59161d3176e63c
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-14 21:21:31 +0530

    Mark the startup methods as virtual
    This allows us to be able to override its behaviour in the test projects.

--- src/EventManagement.Web/Startup.cs
+++ src/EventManagement.Web/Startup.cs
@@ -33,7 +33,7 @@ namespace losol.EventManagement
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public virtual void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
             {
@@ -138,7 +138,7 @@ namespace losol.EventManagement
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public virtual void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
