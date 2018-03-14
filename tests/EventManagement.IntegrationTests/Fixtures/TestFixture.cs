c0a405f18782fad6ee7ad829931593ca1c377fc0
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-14 21:25:10 +0530

    Subclass Startup and use it in the TestFixture

--- tests/EventManagement.IntegrationTests/Fixtures/TestFixture.cs
+++ tests/EventManagement.IntegrationTests/Fixtures/TestFixture.cs
@@ -1,5 +1,4 @@
 using System;
-using System.IO;
 using System.Net.Http;
 using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
@@ -7,14 +6,14 @@ using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 using losol.EventManagement.Infrastructure;
-using Microsoft.EntityFrameworkCore;
 
 namespace losol.EventManagement.IntegrationTests.Fixtures
 {
-    // A test fixture which hosts the target project (project we wish to test) in an in-memory server.
+    // A test fixture which hosts the target project in an in-memory server.
     public class TestFixture<TStartup> : IDisposable
     {
         private readonly TestServer _server;
@@ -23,13 +22,11 @@ namespace losol.EventManagement.IntegrationTests.Fixtures
 
 		public TestFixture()
         {
-			var startupAssembly = typeof(TStartup).GetTypeInfo().Assembly;
-
 			var builder = new WebHostBuilder()
 				.UseContentRoot("../../../../../src/EventManagement.Web")
-				.ConfigureServices(InitializeServices)
 				.UseEnvironment("Development")
-				.UseStartup(typeof(TStartup));
+				.UseStartup<TestStartup>()
+				.ConfigureServices(ConfigureServices);
 
 			_server = new TestServer(builder);
 
@@ -44,14 +41,14 @@ namespace losol.EventManagement.IntegrationTests.Fixtures
             _server.Dispose();
         }
 
-        protected virtual void InitializeServices(IServiceCollection services)
+		protected virtual void ConfigureServices(IServiceCollection services)
         {
-			services.AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+			services.AddEntityFrameworkInMemoryDatabase();
 			services.AddDbContext<ApplicationDbContext>(options => {
 				options.UseInMemoryDatabase("losol-eventmanagement-itests");
 			});
 
-            var startupAssembly = typeof(TStartup).GetTypeInfo().Assembly;
+			var startupAssembly = typeof(Startup).GetTypeInfo().Assembly;
             var manager = new ApplicationPartManager();
             manager.ApplicationParts.Add(new AssemblyPart(startupAssembly));
             manager.FeatureProviders.Add(new ControllerFeatureProvider());
