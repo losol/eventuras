79b60087398e5995cc76ebc65bc0a678f6a21e75
Ole Kristian Losvik <losvik+github@gmail.com>
2018-03-15 07:57:12 +0100

    From float to decimal

---
+++ src/EventManagement.Infrastructure/Migrations/20180315065632_no_floating.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace losol.EventManagement.Infrastructure.Migrations
+{
+    public partial class no_floating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "ProductVariants",
+                nullable: false,
+                oldClrType: typeof(float));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "ProductVariants",
+                nullable: false,
+                oldClrType: typeof(decimal));
+        }
+    }
+}
