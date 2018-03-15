736da42b5dd9e821d484466b607de831ace73149
Ole Kristian Losvik <losvik+github@gmail.com>
2018-03-15 21:43:36 +0100

    Updated

--- src/EventManagement.Infrastructure/ApplicationDbContext.cs
+++ src/EventManagement.Infrastructure/ApplicationDbContext.cs
@@ -15,16 +15,9 @@ namespace losol.EventManagement.Infrastructure
         {
         }
 
-
-
-
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
-
         }
 
         public DbSet<losol.EventManagement.Domain.ApplicationUser> ApplicationUsers { get; set; }
