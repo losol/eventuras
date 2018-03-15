508fc8df04b54e03d6b61e4433b0e87d2fc8be7f
Ole Kristian Losvik <losvik+github@gmail.com>
2018-03-16 00:30:52 +0100

    Added two apis

---
+++ src/EventManagement.Web/Controllers/Api/EventsController.cs
@@ -0,0 +1,40 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using losol.EventManagement.Domain;
+using losol.EventManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace losol.EventManagement.Web.Api.Controllers
+{
+	[Route("api/v0/events")]
+	public class EventsController : Controller
+	{
+		private readonly IEventInfoService _eventsService;
+
+		public EventsController(IEventInfoService eventsService)
+		{
+			_eventsService = eventsService;
+		}
+
+		[HttpGet, Route("upcoming")]
+		public async Task<IActionResult> Get()
+		{
+			var events = await _eventsService.GetUpcomingEventsAsync();
+			var list = events.Select(s => new
+			{
+				s.Title,
+				s.Description,
+				s.Location,
+				s.City,
+				s.DateStart,
+				s.DateEnd,
+				s.FeaturedImageUrl
+			});
+			return Ok(list);
+		}
+
+	}
+}
