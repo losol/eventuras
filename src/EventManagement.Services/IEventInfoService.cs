25431490dce99f2d694d0df14ae7f551b4ac0d59
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-16 11:30:02 +0530

    Expose a registration count method

--- src/EventManagement.Services/IEventInfoService.cs
+++ src/EventManagement.Services/IEventInfoService.cs
@@ -9,6 +9,7 @@ namespace losol.EventManagement.Services
 	{
 		Task<EventInfo> GetAsync(int id);
 		Task<EventInfo> GetWithProductsAsync(int id);
+		Task<int> GetVerifiedRegistrationCount(int eventId);
 
 		Task<List<EventInfo>> GetFeaturedEventsAsync();
 		Task<List<EventInfo>> GetUpcomingEventsAsync();
