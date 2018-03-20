1390d0a826f9607e2f416cddae98c701b7f40528
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-20 11:39:19 +0530

    Create a simple OrderService

---
+++ src/EventManagement.Services/OrderService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using losol.EventManagement.Domain;
+using losol.EventManagement.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace losol.EventManagement.Services
+{
+	public class OrderService : IOrderService
+	{
+		private readonly ApplicationDbContext _db;
+
+		public OrderService(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		public Task<List<Order>> GetAsync()
+		{
+			return _db.Orders
+				      .OrderByDescending(o => o.OrderTime)
+					  .ToListAsync();
+		}
+
+		public Task<List<Order>> GetAsync(int count, int offset)
+		{
+			return _db.Orders
+				      .Skip(offset)
+				      .Take(count)
+				      .OrderByDescending(o => o.OrderTime)
+					  .ToListAsync();
+		}
+		public Task<List<Order>> GetAsync(int count) => GetAsync(0, count);
+	}
+}
