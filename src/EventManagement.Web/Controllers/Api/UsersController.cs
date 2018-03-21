40958d4afd232193fca7a27986e62cc0e3d481a8
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-03-21 10:06:33 +0530

    Add a Users Controller

---
+++ src/EventManagement.Web/Controllers/Api/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using losol.EventManagement.Domain;
+using losol.EventManagement.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace losol.EventManagement.Web.Controllers.Api
+{
+	[Route("api/v0/users")]
+	[Authorize]
+	public class UsersController : Controller
+	{
+		private readonly ApplicationDbContext _db; // TODO: Get rid of this
+		public UsersController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		[HttpGet("")]
+		public async Task<IActionResult> GetUsers() 
+		{
+			var users = await _db.Users
+			                     .Select(u => new { Name = u.Name, Email = u.Email })
+			                     .ToListAsync();
+			return Ok(users);
+		}
+	}
+}
