84242aebf2f0419ea480ba65db1d0955e22b12dc
Ole Kristian Losvik <losvik+github@gmail.com>
2018-03-31 11:59:22 +0200

    Added admin interface for users

--- src/EventManagement.Web/Controllers/Api/UsersController.cs
+++ src/EventManagement.Web/Controllers/Api/UsersController.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore;
 namespace losol.EventManagement.Web.Controllers.Api
 {
 	[Route("api/v0/users")]
-	[Authorize]
+	[Authorize(Policy = "AdministratorRole")]
 	public class UsersController : Controller
 	{
 		private readonly ApplicationDbContext _db; // TODO: Get rid of this
@@ -25,7 +25,7 @@ namespace losol.EventManagement.Web.Controllers.Api
 		public async Task<IActionResult> GetUsers() 
 		{
 			var users = await _db.Users
-			                     .Select(u => new { Name = u.Name, Email = u.Email })
+			                     .Select(u => new { Name = u.Name, Email = u.Email, Phone = u.PhoneNumber })
 			                     .ToListAsync();
 			return Ok(users);
 		}
