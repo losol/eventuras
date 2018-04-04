0cc6c34074d9f20893a732c815ba71cd93163752
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-04-04 16:44:24 +0530

    Certificate preview for events

--- src/EventManagement.Web/Controllers/CertificateController.cs
+++ src/EventManagement.Web/Controllers/CertificateController.cs
@@ -9,6 +9,9 @@ using losol.EventManagement.Domain;
 using losol.EventManagement.Web.ViewModels.Templates;
 using losol.EventManagement.Web.Services;
 using Microsoft.AspNetCore.Authorization;
+using losol.EventManagement.Services.Messaging;
+using losol.EventManagement.ViewModels;
+using losol.EventManagement.Services;
 
 namespace EventManagement.Web.Controllers
 {
@@ -20,6 +23,18 @@ namespace EventManagement.Web.Controllers
         public IActionResult ViewCertificate([FromRoute]int id)
         {
             return View("Templates/Certificates/CourseCertificate", CertificateVM.Mock);
+        }
+
+        [HttpGet("for_event/{id}")]
+        public async Task<IActionResult> ViewCertificateForEvent([FromRoute]int id,
+            [FromServices]IEventInfoService eventInfoService)
+        {
+            var eventInfo = await eventInfoService.GetAsync(id);
+            var vm = CertificateVM.Mock;
+            vm.Title = eventInfo.Title;
+            vm.Date = eventInfo.DateEnd?.ToString("dd.MM.yyyy");
+            vm.City = eventInfo.City;
+            return View("Templates/Certificates/CourseCertificate", vm);
         }
 
         [HttpGet("{id}/download")]
