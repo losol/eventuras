b7e19c293f219907886fce718268e4da98ff3c49
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-04-04 13:13:38 +0530

    Extend the email sender interface to support email attachements

--- src/EventManagement.Services/Messaging/IEmailSender.cs
+++ src/EventManagement.Services/Messaging/IEmailSender.cs
@@ -8,5 +8,12 @@ namespace losol.EventManagement.Services.Messaging
     public interface IEmailSender
     {
         Task SendEmailAsync(string email, string subject, string message);
+        Task SendEmailAsync(string email, string subject, string message, Attachment attachment);
+    }
+
+    public class Attachment
+    {
+        public string Filename { get; set; }
+        public byte[] Bytes { get; set; }
     }
 }
