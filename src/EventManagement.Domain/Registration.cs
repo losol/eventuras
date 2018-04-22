12a638d8fa7574afa11f4be3b12a7ee72a7fab82
Ole Kristian Losvik <losvik+github@gmail.com>
2018-04-22 22:31:45 +0200

    Added hascertificate

--- src/EventManagement.Domain/Registration.cs
+++ src/EventManagement.Domain/Registration.cs
@@ -80,6 +80,7 @@ namespace losol.EventManagement.Domain
 		}
 
 		public bool HasOrder => Orders != null && Orders.Count > 0;
+		public bool HasCertificate => Certificate != null;
 
 		public void CreateOrder(IEnumerable<Product> products, IEnumerable<ProductVariant> variants)
 		{
