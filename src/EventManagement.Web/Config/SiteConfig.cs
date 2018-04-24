2aa6c9d517e4342311d523d3b2a4165a0d0819ca
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-04-24 09:20:38 +0530

    Add a SiteConfig class

---
+++ src/EventManagement.Web/Config/SiteConfig.cs
@@ -0,0 +1,8 @@
+namespace losol.EventManagement.Web.Config
+{
+    public class SiteConfig
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
