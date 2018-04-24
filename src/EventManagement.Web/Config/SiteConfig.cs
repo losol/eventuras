b4d431803f61ff72c3ae981fdee291b26fbdb5a5
Ole Kristian Losvik <losvik+github@gmail.com>
2018-04-24 19:35:55 +0200

    Renamed to site

--- src/EventManagement.Web/Config/SiteConfig.cs
+++ src/EventManagement.Web/Config/SiteConfig.cs
@@ -1,6 +1,6 @@
 namespace losol.EventManagement.Web.Config
 {
-    public class SiteConfig
+    public class Site
     {
         public string Title { get; set; }
         public string Description { get; set; }
