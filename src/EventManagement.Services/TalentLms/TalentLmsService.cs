105c430e35509d477b1febac601fe779ac70d9d3
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-04-25 20:10:44 +0530

    Basic TalentLmsService implementation

---
+++ src/EventManagement.Services/TalentLms/TalentLmsService.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using EventManagement.Services.Extensions;
+using losol.EventManagement.Services.TalentLms.Models;
+using Microsoft.Extensions.Options;
+using RestSharp;
+
+namespace losol.EventManagement.Services.TalentLms
+{
+    public class TalentLmsService
+    {
+
+        private readonly TalentLmsOptions _options;
+        public TalentLmsService(IOptions<TalentLmsOptions> options)
+        {
+            _options = options.Value;
+        }
+
+        public async Task<User> CreateUser(User user)
+        {
+            var client = new RestClient($"{_options.BaseUrl}/v1/usersignup");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", $"Basic {_options.ApiKeySha}");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddBody(user);
+            var response = await client.ExecuteAsync<User>(request);
+            return response.Data;
+        }
+
+        public async Task EnrolUserToCourse(User user)
+        {
+            var client = new RestClient($"{_options.BaseUrl}/v1/addusertocourse");
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", $"Basic {_options.ApiKeySha}");
+            request.AddHeader("Content-Type", "application/json");
+            var requestBody = new 
+            {
+                user_id = 2,
+                course_id = 126,
+                role = "learner"
+            };
+            request.AddBody(requestBody);
+            await client.ExecuteAsync(request);
+        }
+    }
+}
