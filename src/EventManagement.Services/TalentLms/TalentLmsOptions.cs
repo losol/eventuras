17a74ec68484ee2b1fad968987ce5281ee8ff928
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-04-25 20:10:44 +0530

    Basic TalentLmsService implementation

---
+++ src/EventManagement.Services/TalentLms/TalentLmsOptions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Text;
+
+namespace losol.EventManagement.Services.TalentLms
+{
+    public class TalentLmsOptions
+    {
+        public string Domain { get; set; }
+        public string ApiKey { get; set; }
+        public string BaseUrl => $"https://{Domain}/api/v1";
+        public string ApiKeySha => 
+            Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(ApiKey));
+    }
+}
