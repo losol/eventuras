f12379107c1093407be39f6ff403fb24e3eebe99
Ole Kristian Losvik <losvik+github@gmail.com>
2018-04-26 22:22:42 +0200

    Added service for getting registrations for user

--- src/EventManagement.Services/RegistrationService.cs
+++ src/EventManagement.Services/RegistrationService.cs
@@ -210,36 +210,12 @@ namespace losol.EventManagement.Services {
 			return await _db.SaveChangesAsync() > 0;
 		}
 
-
-		/* 
-		private async Task<bool> ConfirmRegistrationEmail(Registration registration)
-		{
-			// Prepare an email to send out
-			var emailVM = new EmailMessage()
-			{
-				Name = Registration.ParticipantName,
-				Email = Registration.Email,
-				Subject = "Du var allerede påmeldt!",
-				Message = @"Vi hadde allerede registrert deg i systemet.
-								Ta kontakt med ole@nordland-legeforening hvis du tror det er skjedd noe feil her!
-								"
-			};
-
-			// If registered but not verified, just send reminder of verification. 
-			if (registration.Verified == false)
-			{
-				var verificationUrl = Url.Action("Confirm", "Register", new { id = registration.RegistrationId, auth = registration.VerificationCode }, protocol: Request.Scheme);
-				emailVM.Subject = "En liten bekreftelse bare...";
-				emailVM.Message = $@"Vi hadde allerede registrert deg i systemet, men du har ikke bekreftet enda.
-								<p><a href='{verificationUrl}'>Bekreft her</a></p>
-								<p></p>
-								<p>Hvis lenken ikke virker, så kan du kopiere inn teksten under i nettleseren:
-								{verificationUrl} </p>";
-			}
-
-			await _standardEmailSender.SendAsync(emailVM);
-			return RedirectToPage("/Info/EmailSent");
+		public async Task<List<Registration>> GetRegistrationsWithOrders(ApplicationUser user) {
+			var reg = await _db.Registrations
+				.Where( m => m.UserId == user.Id)
+				.Include ( m=> m.Orders)
+				.ToListAsync();
+			return reg;
 		}
-		 */
 	}
 }
