9549bcd6a5bb45afef967796ca0c508328942e13
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-04-26 10:19:33 +0530

    Add method to fetch user and create user only if it doesn't already exist

--- src/EventManagement.Services/TalentLms/MockTalentLmsService.cs
+++ src/EventManagement.Services/TalentLms/MockTalentLmsService.cs
@@ -5,12 +5,19 @@ namespace losol.EventManagement.Services.TalentLms
 {
     public class MockTalentLmsService : ITalentLmsService
     {
-        public async Task<User> CreateUser(User user) => await Task.FromResult(user);
+        public async Task<User> CreateUser(User user) => 
+            await Task.FromResult(user);
+
+        public async Task<User> CreateUserIfNotExists(User user) => 
+            await Task.FromResult(user);
 
         public async Task EnrolUserToCourse(int userId, int courseId) => 
             await Task.FromResult(0);
 
         public async Task<string> GetCourseLink(int userId, int courseId) => 
             await Task.FromResult<string>(null);
+
+        public async Task<User> GetUserAsync(string email) => 
+            await Task.FromResult<User>(null);
     }
 }
