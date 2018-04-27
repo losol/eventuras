b58e005ec7059103805aa85b6cf25b8e7b4b63d4
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-04-27 14:00:01 +0530

    Implement a MagicLinkEmailSender

--- src/EventManagement.Web/Services/MagicLinkSender.cs
+++ src/EventManagement.Web/Services/MagicLinkSender.cs
@@ -1,33 +1,58 @@
 using System.Threading.Tasks;
+using losol.EventManagement.Domain;
 using losol.EventManagement.Services.Messaging;
 using losol.EventManagement.Web.Config;
 using losol.EventManagement.Web.ViewModels.Email;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace losol.EventManagement.Web.Services
 {
     public sealed class MagicLinkSender : ApplicationEmailSender
     {
+        private readonly IUrlHelper _urlHelper;
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly Site _siteConfig;
+        private readonly string _requestScheme;
 
-        public MagicLinkSender(IEmailSender emailSender, IRenderService renderService,
-         Site siteConfig) 
+        public MagicLinkSender(
+                IEmailSender emailSender, 
+                IRenderService renderService,
+                IUrlHelperFactory urlHelperFactory,
+                IActionContextAccessor actionContextAccessor,
+                UserManager<ApplicationUser> userManager,
+                Site siteConfig) 
             : base(emailSender, renderService) 
         {
-            this._siteConfig = siteConfig;
+            _urlHelper = urlHelperFactory.GetUrlHelper(actionContextAccessor.ActionContext);;
+            _requestScheme = actionContextAccessor.ActionContext.HttpContext.Request.Scheme;
+            _userManager = userManager;
+            _siteConfig = siteConfig;
         }
 
         protected override string Template => "Templates/Email/MagicLinkEmail";
         
-        public async Task SendAsync(string emailAddress)
+        public async Task SendAsync(ApplicationUser user)
         {
+            var token = await _userManager.GenerateUserTokenAsync(
+                user: user,  
+                tokenProvider: "MagicLinkTokenProvider", 
+                purpose: "magic-link"
+            );
+            var magiclink = _urlHelper.Link(
+                        routeName: "MagicLinkRoute", 
+                        values: new { token = token, email = user.Email });
             await base.SendAsync(
-                emailAddress: emailAddress, 
+                emailAddress: user.Email, 
                 subject: $"MagicLink to log into {_siteConfig.Title}",
                 vm: new MagicLinkVM
                 {
-                    MagicLink = "https://google.com"
+                    MagicLink = magiclink
                 }
             );
+            
         }
     }
 }
