e566cae70a2048f3c111ba337582e890abf5062d
Ole Kristian Losvik <losvik+github@gmail.com>
2018-04-30 18:40:03 +0200

    Added ef migration

---
+++ src/EventManagement.Infrastructure/Migrations/20180430163747_attended_remove.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace losol.EventManagement.Infrastructure.Migrations
+{
+    public partial class attended_remove : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Attended",
+                table: "Registrations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Attended",
+                table: "Registrations",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
