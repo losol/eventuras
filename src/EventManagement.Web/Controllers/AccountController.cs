1795a2348345cfe6f8e4f6cecbc96f9325117170
Ole Kristian Losvik <losvik+github@gmail.com>
2018-05-01 16:23:29 +0200

    changed from route to query

--- src/EventManagement.Web/Controllers/AccountController.cs
+++ src/EventManagement.Web/Controllers/AccountController.cs
@@ -31,7 +31,7 @@ namespace losol.EventManagement.Controllers
         }
 
         [HttpGet("/magic/{userid}/{token}", Name = "MagicLinkRoute")]
-        public async Task<IActionResult> MagicLogin([FromRoute]string userid, [FromRoute]string token )
+        public async Task<IActionResult> MagicLogin([FromQuery]string userid, [FromQuery]string token )
         {
             // Sign the user out if they're signed in
             if(_signInManager.IsSignedIn(User))
