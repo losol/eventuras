a1146f7dd5943dd28d69b37950505311af27f3e8
Ole Kristian Losvik <losvik+github@gmail.com>
2018-05-02 14:46:06 +0200

    Added tag helper

---
+++ src/EventManagement.Web/TagHelpers/RegistationTypeTagHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using static losol.EventManagement.Domain.Order;
+using static losol.EventManagement.Domain.Registration;
+
+namespace losol.EventManagement.Web.TagHelpers
+{
+	[HtmlTargetElement("order-status", TagStructure = TagStructure.WithoutEndTag)]
+	public class RegistationTypeTagHelper : TagHelper
+	{
+		public RegistrationType Value { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagMode = TagMode.StartTagAndEndTag;
+			output.TagName = "span";
+			output.Content.SetContent(Value.ToString());
+
+			TagHelperAttribute badgeClass;
+			switch (Value)
+			{
+				case RegistrationType.Participant:
+					badgeClass = new TagHelperAttribute("class", "badge badge-light");
+					break;
+				case RegistrationType.Staff:
+					badgeClass = new TagHelperAttribute("class", "badge badge-warning");
+					break;
+				case RegistrationType.Student:
+					badgeClass = new TagHelperAttribute("class", "badge badge-info");
+					break;
+				case RegistrationType.Lecturer:
+					badgeClass = new TagHelperAttribute("class", "badge badge-success");
+					break;
+				default:
+					badgeClass = new TagHelperAttribute("class", "badge badge-secondary");
+					break;
+			}
+
+			output.Attributes.SetAttribute(badgeClass);
+		}
+	}
+}
