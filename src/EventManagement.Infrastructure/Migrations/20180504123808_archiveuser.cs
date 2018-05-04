a02a1d9634739364b6166293e46832397255b816
Ole Kristian Losvik <losvik+github@gmail.com>
2018-05-04 15:26:23 +0200

    Registrations and orders for user (#188)
    * Added registrations to admin user profile page
    
    * Added registration status and type
    
    * Add archived field for user
    
    * Added migration for archiving users
    
    * Archive instead of deleting user
    
    * New text.

---
+++ src/EventManagement.Infrastructure/Migrations/20180504123808_archiveuser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace losol.EventManagement.Infrastructure.Migrations
+{
+    public partial class archiveuser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Archived",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Archived",
+                table: "AspNetUsers");
+        }
+    }
+}
