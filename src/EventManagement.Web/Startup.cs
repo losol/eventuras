bd5e55d0e53b1328ae258463aaef3135037044bf
Ole Kristian Losvik <losvik+github@gmail.com>
2018-05-10 07:03:33 +0200

    Changed error url

--- src/EventManagement.Web/Startup.cs
+++ src/EventManagement.Web/Startup.cs
@@ -209,7 +209,7 @@ namespace losol.EventManagement
             }
             else
             {
-                app.UseExceptionHandler("Info/Error");
+                app.UseExceptionHandler("/Info/Error");
             }
 
             app.UseStaticFiles();
