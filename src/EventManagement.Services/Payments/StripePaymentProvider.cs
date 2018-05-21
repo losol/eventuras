6baf162a3ad953606202927fb9ece7b03fb577b8
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-05-21 15:16:36 +0530

    [WIP] Stripe integration for direct payments (#220)
    * Add a StripeDirect payment method
    
    * Add a StripeDirect PaymentProvider enum constant
    
    * Copy the sample code over
    
    * Copy the payment widget over
    
    * Get the stripe element to look right
    
    * Make a test payment for orderno 6
    
    The order is not marked as paid after the payment is done!

---
+++ src/EventManagement.Services/Payments/StripePaymentProvider.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using System.Threading.Tasks;
+using losol.EventManagement.Domain;
+using Stripe;
+
+namespace EventManagement.Services.Payments
+{
+    public class StripePaymentProvider
+    {
+
+        public async Task ChargeCustomer(Order order, StripeToken token)
+        {
+            var options = new StripeChargeCreateOptions {
+                Amount = (int)(order.TotalAmount * 100m),
+                Currency = "nok",
+                Description = order.Registration.EventInfo.Title,
+                SourceTokenOrExistingSourceId = token.Id,
+                ReceiptEmail = order.CustomerEmail
+            };
+            var service = new StripeChargeService();
+            StripeCharge charge = await service.CreateAsync(options);
+        }
+
+        private async Task<StripeCustomer> getOrCreateCustomer(string email)
+        {
+
+            var service = new StripeCustomerService();
+            var listOptions = new StripeCustomerListOptions
+            {
+                Limit = 1
+            };
+            listOptions.AddExtraParam("email", email);
+            var customer = (await service.ListAsync(listOptions)).Data.FirstOrDefault();
+            if (customer != null) return customer;
+
+            var customerCreateOptions = new StripeCustomerCreateOptions
+            {
+                Email = email
+            };
+            return await service.CreateAsync(customerCreateOptions);
+        }
+    }
+}
