2bdd813b73470954d1d9de17f0bcad7310daef7b
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-21 19:28:00 +0530

    Fetch the event in the POC page
    New route url: http://localhost:5000/Events/Participants/POC/1/cipa-meeting-the-cardiac-safety-research-consortium

--- src/EventManagement.Web/Pages/Events/Participants/POCModel.cshtmlc.s.cs
+++ src/EventManagement.Web/Pages/Events/Participants/POCModel.cshtmlc.s.cs
@@ -1,5 +1,8 @@
 using System.Threading.Tasks;
+using losol.EventManagement.Domain;
+using losol.EventManagement.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace losol.EventManagement.Pages.Events.Participants
@@ -8,10 +11,33 @@ namespace losol.EventManagement.Pages.Events.Participants
     public class POCModel : PageModel
     {
         public string Slug { get; private set; }
-        public async Task<PageResult> OnGetAsync(string slug)
+        public EventInfo EventInfo { get; private set; }
+        private readonly IAuthorizationService _authService;
+        private readonly IEventInfoService _eventsService;
+
+        public POCModel(IAuthorizationService authService,
+                        IEventInfoService eventsService)
+        {
+            _authService = authService;
+            _eventsService = eventsService;
+        }
+
+        public async Task<IActionResult> OnGetAsync(int id, string slug)
         {
             this.Slug = slug;
-            await Task.FromResult(0);
+
+            EventInfo = await _eventsService.GetWithProductsAsync(id);
+
+            if (EventInfo == null)
+            {
+                return NotFound();
+            }
+
+            if(EventInfo.Code != slug)
+            {
+                return RedirectToPage("./POC", new { id, slug = EventInfo.Code });
+            }
+
             return Page();
         }
     }
