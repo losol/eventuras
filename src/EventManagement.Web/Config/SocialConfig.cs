7dfcf64ae1a7a9643d404dda41b810932ee37aed
Ole Kristian Losvik <losvik+github@gmail.com>
2018-08-28 22:44:46 +0200

    Adding social settings

---
+++ src/EventManagement.Web/Config/SocialConfig.cs
@@ -0,0 +1,11 @@
+namespace losol.EventManagement.Web.Config
+{
+    public class Social
+    {
+        public class Facebook
+        {
+            public string AppId { get; set; }
+            public string AppSecret { get; set; }
+        }
+    }
+}
