7293a7535cdba774f0e3c303e27476f9a526eda3
Galdin Raphael <gldraphael@users.noreply.github.com>
2018-08-29 12:20:49 +0530

    Add a FacebookConfig property under Social

--- src/EventManagement.Web/Config/SocialConfig.cs
+++ src/EventManagement.Web/Config/SocialConfig.cs
@@ -2,7 +2,9 @@ namespace losol.EventManagement.Web.Config
 {
     public class Social
     {
-        public class Facebook
+        public FacebookConfig Facebook { get; set; }
+
+        public class FacebookConfig
         {
             public string AppId { get; set; }
             public string AppSecret { get; set; }
