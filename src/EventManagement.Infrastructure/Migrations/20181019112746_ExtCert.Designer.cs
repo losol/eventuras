170b450f5e96fc5d48f6183a72b261dffdd2e133
Ole Kristian Losvik <losvik+github@gmail.com>
2018-10-19 13:28:56 +0200

    Added migration for certifcate status

--- src/EventManagement.Infrastructure/Migrations/20181019112746_ExtCert.Designer.cs
+++ src/EventManagement.Infrastructure/Migrations/20181019112746_ExtCert.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using losol.EventManagement.Infrastructure;
 
 namespace losol.EventManagement.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181019112746_ExtCert")]
+    partial class ExtCert
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
