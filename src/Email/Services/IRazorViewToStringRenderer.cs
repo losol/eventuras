e56af80ba5709c3e21fc3d5c27e601ac60568f69
Ole Kristian Losvik <ole@losol.no>
2019-10-27 10:03:27 +0100

    Added razor view to string renderer

---
+++ src/Email/Services/IRazorViewToStringRenderer.cs
@@ -0,0 +1,22 @@
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Losol.Communication.Email.Services
+{
+    public interface IRazorViewToStringRenderer
+    {
+        Task<string> RenderViewToStringAsync<TModel>(string viewName, TModel model);
+    }
+}
