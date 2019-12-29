4d495e0b9662f186f491973b680ad4aab8e65378
Andrew Anisimov <anisimov.new@gmail.com>
2019-12-29 23:23:11 +0300

    [WIP] SMS sending code from Eventuras

---
+++ src/Losol.Communication.Sms.Twilio/TwilioOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Losol.Communication.Sms.Twilio
+{
+    public class TwilioOptions
+    {
+        public string From { get; set; }
+        public string Sid { get; set; }
+        public string AuthToken { get; set; }
+    }
+}
