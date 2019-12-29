34bd443577f18dede7ee5244f8887f03774a7e85
Andrew Anisimov <anisimov.new@gmail.com>
2019-12-30 01:40:22 +0300

    [WIP] Mock services.

---
+++ src/Losol.Communication.Sms.Mock/MockSmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Losol.Communication.Sms.Mock
+{
+    public class MockSmsSender : ISmsSender
+    {
+        public Task SendSmsAsync(string to, string body)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Losol.Communication.Sms.Mock/TwilioSmsServicesExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Losol.Communication.Sms.Mock
+{
+    public static class MockSmsServicesExtensions
+    {
+        public static IServiceCollection AddMockSmsServices(this IServiceCollection services)
+        {
+            services.TryAddTransient<ISmsSender, MockSmsSender>();
+            return services;
+        }
+    }
+}
