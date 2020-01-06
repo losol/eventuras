5aecf121980ad580ab58aa859fb7cdc8de7ef858
Ole Kristian Losvik <losvik+github@gmail.com>
2020-01-06 19:38:18 +0100

    Added Sortable as npm package * Upgraded Sortablejs

--- src/EventManagement.Web/ClientApp/app.js
+++ src/EventManagement.Web/ClientApp/app.js
@@ -3,5 +3,8 @@ window.jQuery = $;
 window.$ = $;
 import "bootstrap";
 import "./sass/main.scss";
+
+// Admin only (TODO: move to separate build)
 import "bootstrap-3-typeahead";
-import "./sortable.js";
+import Sortable from "sortablejs";
+window.Sortable = Sortable;
