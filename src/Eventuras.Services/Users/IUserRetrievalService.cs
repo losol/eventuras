c3c734cbdc4a6a4bc05d942c6b918a0a0edcd8ef
Andrew Anisimov <anisimov.new@gmail.com>
2021-01-06 18:53:04 +0300

    Fix user list retrieval.
    Don't check for org membership (make it optional).

--- src/Eventuras.Services/Users/IUserRetrievalService.cs
+++ src/Eventuras.Services/Users/IUserRetrievalService.cs
@@ -8,6 +8,15 @@ namespace Eventuras.Services.Users
     {
         Task<ApplicationUser> GetUserByIdAsync(string userId);
 
-        Task<List<ApplicationUser>> ListAccessibleUsers(UserRetrievalOptions options = null);
+        Task<List<ApplicationUser>> ListUsers(UserFilter filter = null, UserRetrievalOptions options = null);
+    }
+
+    public class UserFilter
+    {
+        /// <summary>
+        /// Whether to load only users accessible to the currently signed
+        /// in user via org membership.
+        /// </summary>
+        public bool AccessibleToOrgOnly { get; set; }
     }
 }
