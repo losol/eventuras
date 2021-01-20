5a7ec98f7fd584a45d0bb70d46f7a2a146464289
Andrew Anisimov <anisimov.new@gmail.com>
2021-01-20 14:31:47 +0300

    Fix failed tests.
    Caused by unhandled exceptions.

---
+++ src/Eventuras.Web/HttpResponseExceptionFilter.cs
@@ -0,0 +1,36 @@
+using System;
+using Eventuras.Services.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Eventuras.Web
+{
+    public class HttpResponseExceptionFilter : IActionFilter, IOrderedFilter
+    {
+        public int Order { get; } = int.MaxValue - 10;
+
+        public void OnActionExecuting(ActionExecutingContext context) { }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var result = GetResult(context.Exception);
+            if (result == null)
+            {
+                return;
+            }
+            context.Result = result;
+            context.ExceptionHandled = true;
+        }
+
+        private static IActionResult GetResult(Exception e)
+        {
+            return e switch
+            {
+                NotFoundException => new NotFoundObjectResult(e.Message),
+                NotAccessibleException => new ForbidResult(),
+                DuplicateException => new ConflictObjectResult(e.Message),
+                _ => null
+            };
+        }
+    }
+}
