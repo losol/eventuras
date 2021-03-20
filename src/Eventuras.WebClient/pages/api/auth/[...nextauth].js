01efc708bbbf8252e45bd41fb5c0f0c3d7ba8cfb
Ole Kristian Losvik <losvik@gmail.com>
2021-03-20 22:12:54 +0100

    Updated auth settings

--- src/Eventuras.WebClient/pages/api/auth/[...nextauth].js
+++ src/Eventuras.WebClient/pages/api/auth/[...nextauth].js
@@ -1,12 +1,38 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
-export default NextAuth({
+const nextOptions = {
   providers: [
     Providers.Auth0({
       clientId: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       domain: process.env.AUTH0_DOMAIN,
+      audience: "https://eventuras/api",
+      scope: "openid profile email registrations:read",
     }),
   ],
-});
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    jwt: true,
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  },
+  callbacks: {
+    async jwt(token, account) {
+      if (account?.access_token) {
+        token.access_token = account.access_token;
+      }
+      console.log("ttt: " + token);
+      return token;
+    },
+    async session(session, token) {
+      if (token?.access_token) {
+        session.access_token = token.access_token;
+      }
+      return session;
+    },
+  },
+};
+
+export default (req, res) => NextAuth(req, res, nextOptions);
