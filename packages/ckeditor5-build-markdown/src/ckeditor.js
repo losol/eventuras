c6e00498efdfd31a43cd62202557035d36c14bda
Ole Kristian Losvik <ole@losol.no>
2021-03-22 21:48:52 +0100

    Update to ckeditor5 v26

--- packages/ckeditor5-build-markdown/src/ckeditor.js
+++ packages/ckeditor5-build-markdown/src/ckeditor.js
@@ -1,66 +1,53 @@
 /**
- * @license Copyright (c) 2014-2020, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import Markdown from '@ckeditor/ckeditor5-markdown-gfm/src/markdown.js';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import Title from '@ckeditor/ckeditor5-heading/src/title.js';
 
-import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
-import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
-import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor.js";
-import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
-import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
-import Link from "@ckeditor/ckeditor5-link/src/link.js";
-import List from "@ckeditor/ckeditor5-list/src/list.js";
-import Markdown from "@ckeditor/ckeditor5-markdown-gfm/src/markdown.js";
-import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
-import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
-import Table from "@ckeditor/ckeditor5-table/src/table.js";
-import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
-import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation.js";
-
-class MarkdownEditor extends ClassicEditorBase {}
+class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
-MarkdownEditor.builtinPlugins = [
-  Autoformat,
-  BlockQuote,
-  Bold,
-  Essentials,
-  Heading,
-  Italic,
-  Link,
-  List,
-  Markdown,
-  Paragraph,
-  PasteFromOffice,
-  Table,
-  TableToolbar,
-  TextTransformation,
+Editor.builtinPlugins = [
+	Autoformat,
+	BlockQuote,
+	Bold,
+	CodeBlock,
+	Essentials,
+	Heading,
+	Image,
+	Italic,
+	Link,
+	LinkImage,
+	List,
+	Markdown,
+	MediaEmbed,
+	Paragraph,
+	PasteFromOffice,
+	RemoveFormat,
+	Table,
+	TableToolbar,
+	TextTransformation,
+	Title
 ];
 
-// Editor configuration.
-MarkdownEditor.defaultConfig = {
-  toolbar: {
-    items: [
-      "heading",
-      "|",
-      "bold",
-      "italic",
-      "underline",
-      "strikethrough",
-      "|",
-      "link",
-      "bulletedList",
-      "numberedList",
-      "|",
-      "undo",
-      "redo",
-    ],
-  },
-  table: {
-    contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
-  },
-};
-
-export default MarkdownEditor;
+export default Editor;
