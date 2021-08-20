c13959a07007677336fc1157c9bf79a30d02c15f
Ole Kristian Losvik <ole@losol.no>
2021-08-20 22:32:36 +0200

    Formatting

--- src/Eventuras.WebClient/components/communication/MarkdownEditor/MarkdownEditor.tsx
+++ src/Eventuras.WebClient/components/communication/MarkdownEditor/MarkdownEditor.tsx
@@ -3,36 +3,38 @@ import Editor from '@losol/ckeditor5-build-markdown';
 import React from 'react';
 
 const editorConfiguration = {
-    toolbar: [
-        'heading',
-        '|',
-        'bold',
-        'italic',
-        'link',
-        'bulletedList',
-        'numberedList',
-        '|',
-        'blockQuote',
-        '|',
-        'undo',
-        'redo',
-    ],
+  toolbar: [
+    'heading',
+    '|',
+    'bold',
+    'italic',
+    'link',
+    'bulletedList',
+    'numberedList',
+    '|',
+    'blockQuote',
+    '|',
+    'undo',
+    'redo',
+  ],
 };
 
 interface MarkdownEditorProps {
-    data: string;
-    onChange: (data: string) => void;
+  data: string;
+  onChange: (data: string) => void;
 }
 
 const MarkdownEditor = (props: MarkdownEditorProps): JSX.Element => {
-    return (
-        <CKEditor
-            data={props.data}
-            onChange={(event: Event, editor: Editor) => props.onChange(editor.getData())}
-            editor={Editor}
-            config={editorConfiguration}
-        />
-    );
+  return (
+    <CKEditor
+      data={props.data}
+      onChange={(event: Event, editor: Editor) =>
+        props.onChange(editor.getData())
+      }
+      editor={Editor}
+      config={editorConfiguration}
+    />
+  );
 };
 
 export default MarkdownEditor;
