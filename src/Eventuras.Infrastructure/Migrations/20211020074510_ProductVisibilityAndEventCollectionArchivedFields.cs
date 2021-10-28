150bad4bcc7f3a146034facc485177731180d462
Andrew Anisimov <anisimov.new@gmail.com>
2021-10-28 13:31:57 +0300

    Missing DB migration (#524)
    Adding product's Visibility and event collection's Archived properties.

---
+++ src/Eventuras.Infrastructure/Migrations/20211020074510_ProductVisibilityAndEventCollectionArchivedFields.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Eventuras.Infrastructure.Migrations
+{
+    public partial class ProductVisibilityAndEventCollectionArchivedFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Visibility",
+                table: "Products",
+                type: "integer",
+                nullable: false,
+                defaultValue: 1);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalInvoiceId",
+                table: "Invoices",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Archived",
+                table: "EventCollections",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Visibility",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Archived",
+                table: "EventCollections");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExternalInvoiceId",
+                table: "Invoices",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
