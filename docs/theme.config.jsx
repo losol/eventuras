73d6bfa6bd524f20737cf797b3fd375c0b66e663
Ole Kristian Losvik <losvik@gmail.com>
2023-07-17 00:29:55 +0200

    docs: New next.js documentation generator * Added nextra documentation starter from Vercel * Moved existing docs into pages folder

---
+++ docs/theme.config.jsx
@@ -0,0 +1,10 @@
+export default {
+    logo: <span>Eventuras docs</span>,
+    project: {
+        link: "https://github.com/losol/eventuras",
+    },
+    footer: {
+        text: <span>At least we tried.</span>,
+    },
+};
+
