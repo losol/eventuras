2a9a8866d6c12a5a0702358f225b6ba372f3c8d2
Fedir <fedir.kopakov@gmail.com>
2023-07-18 23:06:24 +0700

    feat: add i18n multi language
    Run i18n with Next

--- src/Eventuras.WebFrontend/next.config.js
+++ src/Eventuras.WebFrontend/next.config.js
@@ -1,6 +1,22 @@
+// const nextTranslate = require('next-translate');
+const nextTranslate = require('next-translate-plugin');
+
+const defaultLocale = process.env.NEXT_PUBLIC_DEFAULT_LOCALE;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Internationalization(i18n) Routing
+  // https://nextjs.org/docs/pages/building-your-application/routing/internationalization#sub-path-routing
+  // i18n: {
+  //   locales: ['en-US', 'nb-NO'],
+  //   defaultLocale: defaultLocale ? defaultLocale : 'en-US',
+  // },
 };
 
-module.exports = nextConfig;
+module.exports = nextTranslate({
+  webpack: (nextConfig, { isServer, webpack }) => {
+    return nextConfig;
+  }
+})
