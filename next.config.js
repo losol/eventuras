fba96a3013569f9b859ae47903daba9fbe0f1d25
Fedir <fedir.kopakov@gmail.com>
2023-07-21 03:45:49 +0700

    feat: add i18n multi language
    Add formatLanguageName helper.
    Change language codes en/nb to ISO codes en-US/nb-No.
    Disable Next automatic locale detection and redirect to sub-path.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextTranslate = require('next-translate-plugin');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  i18n: {
+    // Disable automatic locale detection and redirect
+    localeDetection: false,
+  },
 };
 
 module.exports = nextTranslate(nextConfig);
