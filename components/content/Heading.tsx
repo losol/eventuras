8819b89e61cbb955c2ecc3924a92474c8e39a7ea
Ole Kristian Losvik <losvik@gmail.com>
2023-08-05 13:21:11 +0200

    feat: add Heading component (#46)

--- components/content/Heading.tsx
+++ components/content/Heading.tsx
@@ -1,18 +1,46 @@
-import { Title, TitleOrder } from '@mantine/core';
-
-type HeadingProps = {
+export type HeadingProps = {
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   children: React.ReactNode;
+  className?: string;
 };
 
-const Heading = ({ as = 'h1', children }: HeadingProps) => {
-  // cast as prop to Titleorder
-  const headingOrder = parseInt(as.replace('h', '')) as TitleOrder;
+const Heading = (props: HeadingProps) => {
+  const HeadingComponent = props.as ?? 'h1';
+
+  // Define default Tailwind CSS classes for font size and padding
+  let defaultClasses = '';
+
+  // Adjust font size and padding based on heading level
+  if (props.className === undefined || props.className === null) {
+    switch (HeadingComponent) {
+      case 'h1':
+        defaultClasses = ' text-6xl pt-16 pb-6';
+        break;
+      case 'h2':
+        defaultClasses = ' text-3xl pt-16 pb-3';
+        break;
+      case 'h3':
+        defaultClasses = ' text-2xl pt-12 pb-6';
+        break;
+      case 'h4':
+        defaultClasses = ' text-xl pt-3 pb-1';
+        break;
+      case 'h5':
+        defaultClasses = ' text-base pt-2 pb-1';
+        break;
+      case 'h6':
+        defaultClasses = ' text-base pt-1 pb-1';
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <>
-      <Title order={headingOrder} pt={16}>
-        {children}
-      </Title>
+      <HeadingComponent className={props.className ?? defaultClasses}>
+        {props.children}
+      </HeadingComponent>
     </>
   );
 };
