38039c14656805394b6585cb24e0ec42167dd599
Yura Bartish <bartish.yura@gmail.com>
2023-08-08 14:49:52 +0300

    feat: add two new exception types for generic use cases

---
+++ src/Eventuras.Services/Exceptions/ArgumentServiceException.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+
+using System;
+
+namespace Eventuras.Services.Exceptions;
+
+public class ArgumentServiceException : ServiceException
+{
+    public string? ParamName { get; }
+
+    public ArgumentServiceException(string message, string? paramName = null, Exception? innerException = null) : base(message, innerException)
+    {
+        ParamName = paramName;
+    }
+}
