b4245c9ebbbf2f3898bbd775f7a18bacea473835
Yura Bartish <bartish.yura@gmail.com>
2023-08-10 15:13:29 +0300

    feat: finally resolve #601 by adding auto create/update

--- src/Eventuras.Services/Orders/IOrderManagementService.cs
+++ src/Eventuras.Services/Orders/IOrderManagementService.cs
@@ -5,35 +5,51 @@ using System.Threading;
 using System.Threading.Tasks;
 using Eventuras.Domain;
 
-namespace Eventuras.Services.Orders
+namespace Eventuras.Services.Orders;
+
+public interface IOrderManagementService
 {
-    public interface IOrderManagementService
-    {
-        /// <summary>
-        /// Marks order as canceled (soft delete).
-        /// </summary>
-        /// <param name="order">Order to cancel.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        Task CancelOrderAsync(Order order, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Marks order as canceled (soft delete).
+    /// </summary>
+    /// <param name="order">Order to cancel.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    Task CancelOrderAsync(Order order, CancellationToken cancellationToken = default);
 
-        /// <summary>
-        /// Updates order lines in order.
-        /// </summary>
-        /// <param name="order">Order to update.</param>
-        /// <param name="newOrderLines">Full update of lines in the order.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        Task UpdateOrderLinesAsync(Order order, ICollection<OrderLineModel> newOrderLines, CancellationToken cancellationToken = default);
+    /// <summary>
+    /// Updates order lines in order.
+    /// </summary>
+    /// <param name="order">Order to update.</param>
+    /// <param name="newOrderLines">Full update of lines in the order.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    Task UpdateOrderLinesAsync(Order order, ICollection<OrderLineModel> newOrderLines, CancellationToken cancellationToken = default);
 
-        /// <summary>
-        /// Creates a new order for given registration.
-        /// </summary>
-        /// <param name="registrationId">Registration to create order for.</param>
-        /// <param name="orderLines">Order lines.</param>
-        /// <param name="cancellationToken">Cancellation token.</param>
-        /// <returns>Created order.</returns>
-        Task<Order> CreateOrderForRegistrationAsync(
-            int registrationId,
-            ICollection<OrderLineModel> orderLines,
-            CancellationToken cancellationToken = default);
-    }
+    /// <summary>
+    /// Creates a new order for given registration.
+    /// </summary>
+    /// <param name="registrationId">Registration to create order for.</param>
+    /// <param name="orderLines">Order lines.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>Created order.</returns>
+    Task<Order> CreateOrderForRegistrationAsync(
+        int registrationId,
+        ICollection<OrderLineModel> orderLines,
+        CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Creates a new order or updates existing order to make registration have all
+    /// specified <paramref name="expectedOrderLines"/> for all orders in total.
+    /// </summary>
+    /// <remarks>
+    /// Order will be created only if there are no open orders (with status <see cref="Order.OrderStatus.Draft"/>) in registration.
+    /// If registration has multiple open orders, last one by creation date is selected for update.
+    /// </remarks>
+    /// <param name="registrationId">Registration to create or update order for.</param>
+    /// <param name="expectedOrderLines">Order lines for registration in total.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>Created or updated order. Returns null if no actions are to perform.</returns>
+    Task<Order?> AutoCreateOrUpdateOrder(
+        int registrationId,
+        IEnumerable<OrderLineModel> expectedOrderLines,
+        CancellationToken cancellationToken = default);
 }
--- src/Eventuras.Services/Orders/OrderLineModel.cs
+++ src/Eventuras.Services/Orders/OrderLineModel.cs
@@ -1,5 +1,12 @@
 ﻿#nullable enable
 
+using Eventuras.Domain;
+
 namespace Eventuras.Services.Orders;
 
-public record OrderLineModel(int ProductId, int? ProductVariantId, int Quantity);
+public record OrderLineModel(int ProductId, int? ProductVariantId, int Quantity)
+{
+    public OrderLineModel CreateWithInvertedQuantity() => this with {Quantity = -this.Quantity};
+
+    public static OrderLineModel FromOrderLineDomainModel(OrderLine orderLine) => new((int)orderLine.ProductId!, orderLine.ProductVariantId, orderLine.Quantity);
+};
