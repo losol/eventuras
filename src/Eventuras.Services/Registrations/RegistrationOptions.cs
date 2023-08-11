c7295bb29fd8cfe0a67a2de2282c3d9ddd2eca3a
Yura Bartish <bartish.yura@gmail.com>
2023-08-11 17:24:03 +0300

    chore: move classes in separate files

---
+++ src/Eventuras.Services/Registrations/RegistrationOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Eventuras.Services.Registrations;
+
+public class RegistrationOptions
+{
+    /// <summary>
+    /// Create an order with all mandatory products included.
+    /// </summary>
+    public bool CreateOrder { get; set; }
+}
