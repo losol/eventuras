7df2bcf1aeac1b6b897e3a843b66bfd6ff293bca
Yura Bartish <bartish.yura@gmail.com>
2023-08-29 15:47:05 +0300

    test: add tests for policies of registration modification

---
+++ tests/Eventuras.Services.Tests/HttpContextAccessorUtils.cs
@@ -0,0 +1,21 @@
+﻿#nullable enable
+
+using System.Linq;
+using System.Security.Claims;
+
+namespace Eventuras.Services.Tests;
+
+public static class HttpContextAccessorUtils
+{
+    public static ClaimsPrincipal GetUser(string userId, params string[] roles)
+    {
+        var identity = new ClaimsIdentity();
+        identity.AddClaim(new(ClaimTypes.NameIdentifier, userId ));
+        identity.AddClaims(roles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+        var principal = new ClaimsPrincipal();
+        principal.AddIdentity(identity);
+
+        return principal;
+    }
+}
