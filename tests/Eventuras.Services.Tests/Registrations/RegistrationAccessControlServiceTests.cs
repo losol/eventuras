d11663aa5e2f873a28c513ef31cf4a6175462735
Yura Bartish <bartish.yura@gmail.com>
2023-08-29 15:47:05 +0300

    test: add tests for policies of registration modification

---
+++ tests/Eventuras.Services.Tests/Registrations/RegistrationAccessControlServiceTests.cs
@@ -0,0 +1,205 @@
+﻿#nullable enable
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Eventuras.Domain;
+using Eventuras.Services.Events;
+using Eventuras.Services.Exceptions;
+using Eventuras.Services.Organizations;
+using Eventuras.Services.Registrations;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using NodaTime;
+using Xunit;
+using static Eventuras.Services.Tests.HttpContextAccessorUtils;
+
+namespace Eventuras.Services.Tests.Registrations;
+
+public class RegistrationAccessControlServiceTests
+{
+    [Fact]
+    public async Task CheckRegistrationUpdateAccessAsync_WithDefaultEventInfoOptions_PassesForOwner()
+    {
+        // Arrange
+        var userId = Guid.NewGuid().ToString();
+        var user = GetUser(userId);
+        HttpContextAccessor.HttpContext = new DefaultHttpContext { User = user };
+
+        var ei = new EventInfo();
+        var reg = new Registration { UserId = userId, EventInfoId = ei.EventInfoId, EventInfo = ei };
+
+        var eventInfoRetrievalService = ServiceMocks.MockEventInfoRetrievalService(out var eiMock, ei);
+        var organizationAccessorService = Mock.Of<ICurrentOrganizationAccessorService>(MockBehavior.Strict); // should not be called
+
+        var testSubject = new RegistrationAccessControlService(HttpContextAccessor, eventInfoRetrievalService, organizationAccessorService);
+
+        // Act
+        await testSubject.CheckRegistrationUpdateAccessAsync(reg, CancellationToken.None);
+
+        // Assert
+        eiMock.Verify(s => s.GetEventInfoByIdAsync(It.IsAny<int>(), It.IsAny<EventInfoRetrievalOptions>(), It.IsAny<CancellationToken>()),
+            Times.Once);
+        eiMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task CheckRegistrationUpdateAccessAsync_WithDefaultEventInfoOptions_ThrowsForNonOwner()
+    {
+        // Arrange
+        var userId = Guid.NewGuid().ToString();
+        var user = GetUser(userId);
+        HttpContextAccessor.HttpContext = new DefaultHttpContext { User = user };
+        var otherUserId = Guid.NewGuid().ToString();
+
+        var ei = new EventInfo();
+        var reg = new Registration { UserId = otherUserId, EventInfoId = ei.EventInfoId, EventInfo = ei };
+
+        var eventInfoRetrievalService = ServiceMocks.MockEventInfoRetrievalService(out var eiMock, ei);
+        var organizationAccessorService = Mock.Of<ICurrentOrganizationAccessorService>(MockBehavior.Strict); // should not be called
+
+        var testSubject = new RegistrationAccessControlService(HttpContextAccessor, eventInfoRetrievalService, organizationAccessorService);
+
+        // Act
+        await Assert.ThrowsAsync<NotAccessibleException>(
+            async () => await testSubject.CheckRegistrationUpdateAccessAsync(reg, CancellationToken.None));
+
+        // Assert
+        eiMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task CheckRegistrationUpdateAccessAsync_WithDefaultEventInfoOptions_PassesForAdmin()
+    {
+        // Arrange
+        var userId = Guid.NewGuid().ToString();
+        var user = GetUser(userId, Roles.SuperAdmin);
+        HttpContextAccessor.HttpContext = new DefaultHttpContext { User = user };
+        var otherUserId = Guid.NewGuid().ToString();
+
+        var ei = new EventInfo();
+        var reg = new Registration { UserId = otherUserId, EventInfoId = ei.EventInfoId, EventInfo = ei };
+
+        var eventInfoRetrievalService = ServiceMocks.MockEventInfoRetrievalService(out var eiMock, ei);
+        var organizationAccessorService = Mock.Of<ICurrentOrganizationAccessorService>(MockBehavior.Strict); // should not be called
+
+        var testSubject = new RegistrationAccessControlService(HttpContextAccessor, eventInfoRetrievalService, organizationAccessorService);
+
+        // Act
+        await testSubject.CheckRegistrationUpdateAccessAsync(reg, CancellationToken.None);
+
+        // Assert
+        eiMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task CheckRegistrationUpdateAccessAsync_WithAllowedRegistrationEditHours_PassesBeforeDue()
+    {
+        // Arrange
+        var userId = Guid.NewGuid().ToString();
+        var user = GetUser(userId);
+        HttpContextAccessor.HttpContext = new DefaultHttpContext { User = user };
+
+        var policy = new EventInfoOptions.EventInfoRegistrationPolicy { AllowedRegistrationEditHours = 24 };
+        var ei = new EventInfo { Options = new EventInfoOptions { RegistrationPolicy = policy } };
+        var reg = new Registration { UserId = userId, EventInfoId = ei.EventInfoId, EventInfo = ei };
+
+        var eventInfoRetrievalService = ServiceMocks.MockEventInfoRetrievalService(out var eiMock, ei);
+        var organizationAccessorService = Mock.Of<ICurrentOrganizationAccessorService>(MockBehavior.Strict); // should not be called
+
+        var testSubject = new RegistrationAccessControlService(HttpContextAccessor, eventInfoRetrievalService, organizationAccessorService);
+
+        // Act
+        await testSubject.CheckRegistrationUpdateAccessAsync(reg, CancellationToken.None);
+
+        // Assert
+        eiMock.Verify(s => s.GetEventInfoByIdAsync(It.IsAny<int>(), It.IsAny<EventInfoRetrievalOptions>(), It.IsAny<CancellationToken>()),
+            Times.Once);
+        eiMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task CheckRegistrationUpdateAccessAsync_WithAllowedRegistrationEditHours_ThrowsAfterDue()
+    {
+        // Arrange
+        var userId = Guid.NewGuid().ToString();
+        var user = GetUser(userId);
+        HttpContextAccessor.HttpContext = new DefaultHttpContext { User = user };
+
+        var policy = new EventInfoOptions.EventInfoRegistrationPolicy { AllowedRegistrationEditHours = 24 };
+        var ei = new EventInfo { Options = new EventInfoOptions { RegistrationPolicy = policy } };
+        var registrationTime = Instant.FromDateTimeUtc(DateTime.UtcNow.AddDays(-2));
+        var reg = new Registration { UserId = userId, EventInfoId = ei.EventInfoId, EventInfo = ei, RegistrationTime = registrationTime };
+
+        var eventInfoRetrievalService = ServiceMocks.MockEventInfoRetrievalService(out var eiMock, ei);
+        var organizationAccessorService = Mock.Of<ICurrentOrganizationAccessorService>(MockBehavior.Strict); // should not be called
+
+        var testSubject = new RegistrationAccessControlService(HttpContextAccessor, eventInfoRetrievalService, organizationAccessorService);
+
+        // Act
+        await Assert.ThrowsAsync<NotAccessibleException>(
+            async () => await testSubject.CheckRegistrationUpdateAccessAsync(reg, CancellationToken.None));
+
+        // Assert
+        eiMock.Verify(s => s.GetEventInfoByIdAsync(It.IsAny<int>(), It.IsAny<EventInfoRetrievalOptions>(), It.IsAny<CancellationToken>()),
+            Times.Once);
+        eiMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task CheckRegistrationUpdateAccessAsync_WithCancellationDueLimit_PassesBeforeDue()
+    {
+        // Arrange
+        var userId = Guid.NewGuid().ToString();
+        var user = GetUser(userId);
+        HttpContextAccessor.HttpContext = new DefaultHttpContext { User = user };
+
+        var policy = new EventInfoOptions.EventInfoRegistrationPolicy { AllowModificationsAfterCancellationDue = false };
+        var cancellationDue = LocalDate.FromDateTime(DateTime.UtcNow.AddDays(1));
+        var ei = new EventInfo { Options = new EventInfoOptions { RegistrationPolicy = policy }, LastCancellationDate = cancellationDue };
+        var reg = new Registration { UserId = userId, EventInfoId = ei.EventInfoId, EventInfo = ei };
+
+        var eventInfoRetrievalService = ServiceMocks.MockEventInfoRetrievalService(out var eiMock, ei);
+        var organizationAccessorService = Mock.Of<ICurrentOrganizationAccessorService>(MockBehavior.Strict); // should not be called
+
+        var testSubject = new RegistrationAccessControlService(HttpContextAccessor, eventInfoRetrievalService, organizationAccessorService);
+
+        // Act
+        await testSubject.CheckRegistrationUpdateAccessAsync(reg, CancellationToken.None);
+
+        // Assert
+        eiMock.Verify(s => s.GetEventInfoByIdAsync(It.IsAny<int>(), It.IsAny<EventInfoRetrievalOptions>(), It.IsAny<CancellationToken>()),
+            Times.Once);
+        eiMock.VerifyNoOtherCalls();
+    }
+
+    [Fact]
+    public async Task CheckRegistrationUpdateAccessAsync_WithCancellationDueLimit_ThrowsAfterDue()
+    {
+        // Arrange
+        var userId = Guid.NewGuid().ToString();
+        var user = GetUser(userId);
+        HttpContextAccessor.HttpContext = new DefaultHttpContext { User = user };
+
+        var policy = new EventInfoOptions.EventInfoRegistrationPolicy { AllowModificationsAfterCancellationDue = false };
+        var cancellationDue = LocalDate.FromDateTime(DateTime.UtcNow.AddDays(-2));
+        var ei = new EventInfo { Options = new EventInfoOptions { RegistrationPolicy = policy }, LastCancellationDate = cancellationDue };
+        var reg = new Registration { UserId = userId, EventInfoId = ei.EventInfoId, EventInfo = ei };
+
+        var eventInfoRetrievalService = ServiceMocks.MockEventInfoRetrievalService(out var eiMock, ei);
+        var organizationAccessorService = Mock.Of<ICurrentOrganizationAccessorService>(MockBehavior.Strict); // should not be called
+
+        var testSubject = new RegistrationAccessControlService(HttpContextAccessor, eventInfoRetrievalService, organizationAccessorService);
+
+        // Act
+        await Assert.ThrowsAsync<NotAccessibleException>(
+            async () => await testSubject.CheckRegistrationUpdateAccessAsync(reg, CancellationToken.None));
+
+        // Assert
+        eiMock.Verify(s => s.GetEventInfoByIdAsync(It.IsAny<int>(), It.IsAny<EventInfoRetrievalOptions>(), It.IsAny<CancellationToken>()),
+            Times.Once);
+        eiMock.VerifyNoOtherCalls();
+    }
+
+    private static readonly IHttpContextAccessor HttpContextAccessor = new HttpContextAccessor();
+}
---
+++ tests/Eventuras.Services.Tests/ServiceMocks.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading;
+using Eventuras.Domain;
+using Eventuras.Services.Events;
+using Eventuras.Services.Exceptions;
+using Moq;
+
+namespace Eventuras.Services.Tests;
+
+public static class ServiceMocks
+{
+    public static IEventInfoRetrievalService MockEventInfoRetrievalService(out Mock<IEventInfoRetrievalService> mock, params EventInfo[] eventInfos)
+    {
+        mock = new Mock<IEventInfoRetrievalService>();
+
+        mock.Setup(s => s.GetEventInfoByIdAsync(It.IsAny<int>(), It.IsAny<EventInfoRetrievalOptions>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((int id, EventInfoRetrievalOptions _, CancellationToken _) =>
+            {
+                var found = eventInfos.FirstOrDefault(ei => ei.EventInfoId == id) ?? throw new NotFoundException();
+                return found;
+            })
+            .Verifiable();
+
+        mock.Setup(s => s.ListEventsAsync(It.IsAny<EventListRequest>(), It.IsAny<EventInfoRetrievalOptions>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync((EventListRequest request, EventInfoRetrievalOptions _, CancellationToken _) =>
+            {
+                var query = eventInfos.AsQueryable();
+                query = query.UseFilter(request.Filter);
+                query = query.UseOrder(request.Order);
+                return Paging.Create(query, request);
+            })
+            .Verifiable();
+
+        return mock.Object;
+    }
+}
