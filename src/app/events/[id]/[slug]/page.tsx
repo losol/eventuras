d1e567aa7ee0e7d734bdb1d79f75a4403eb71ece
Meinte <meinte@gmail.com>
2023-09-06 15:22:33 +0200

    feat: always redirect event detail pages to the correct slug url (#98)

---
+++ src/app/events/[id]/[slug]/page.tsx
@@ -0,0 +1,6 @@
+/**
+ * Equal to ../page - simply incorporates EventInfo, which fetches the event, and redirects to its correct slug
+ * if required.
+ */
+import EventInfo from '../(components)/EventInfo';
+export default EventInfo;
--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,52 +1,6 @@
-import { EventsService, OpenAPI } from '@losol/eventuras';
-
-import { Container, Layout } from '@/components/layout';
-import Logger from '@/utils/Logger';
-
-import EventDetails from './(components)/EventDetails';
-
-type EventInfoProps = {
-  params: {
-    id: number;
-  };
-};
-OpenAPI.BASE = process.env.API_BASE_URL!;
-OpenAPI.VERSION = process.env.NEXT_PUBLIC_API_VERSION!;
-const l = { namespace: 'events:detail' };
-async function getEvents() {
-  return await EventsService.getV3Events({}).catch(e => {
-    Logger.error(l, 'Error fetching events:', e);
-    return null;
-  });
-}
-
-async function getEvent(id: number) {
-  return await EventsService.getV3Events1({ id }).catch(e => {
-    Logger.error(l, 'Error fetching events:', e);
-    return null;
-  });
-}
-
-export async function generateStaticParams() {
-  const events = await getEvents();
-  if (events && events.data && events.data.length) {
-    return events.data.map(event => ({ id: event.id?.toString() }));
-  }
-
-  return [];
-}
-
-const EventInfoPage: React.FC<EventInfoProps> = async ({ params }) => {
-  const eventinfo = await getEvent(params.id);
-  if (!eventinfo) return <div>Event not found</div>;
-
-  return (
-    <Layout>
-      <Container>
-        <EventDetails eventinfo={eventinfo} />
-      </Container>
-    </Layout>
-  );
-};
-
-export default EventInfoPage;
+/**
+ * Equal to ./[slug]/page - simply incorporates EventInfo, which fetches the event, and redirects to its correct slug
+ * if required.
+ */
+import EventInfo from './(components)/EventInfo';
+export default EventInfo;
