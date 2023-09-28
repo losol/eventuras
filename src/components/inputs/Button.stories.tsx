9d9d9f9ef950e53fe9c16d59cdd9cdd76d893463
Ole Kristian Losvik <losvik@gmail.com>
2023-09-29 00:44:08 +0200

    feat: export styles from Button component

--- src/components/inputs/Button.stories.tsx
+++ src/components/inputs/Button.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { buttonStyles } from './Button';
 import Button from './Button';
 
 const meta: Meta<typeof Button> = {
@@ -13,28 +14,28 @@ type Story = StoryObj<typeof Button>;
 
 export const Filled: Story = {
   args: {
-    variant: 'filled',
+    className: `${buttonStyles.base} ${buttonStyles.primary}`,
   },
   render: args => <Button {...args}>Tap Me If You Dare!</Button>,
 };
 
 export const Outline: Story = {
   args: {
-    variant: 'outline',
+    className: `${buttonStyles.base} ${buttonStyles.secondary}`,
   },
   render: args => <Button {...args}>Outline This!</Button>,
 };
 
 export const Light: Story = {
   args: {
-    variant: 'light',
+    className: `${buttonStyles.base} ${buttonStyles.light}`,
   },
   render: args => <Button {...args}>Feather-Light Tap!</Button>,
 };
 
 export const Transparent: Story = {
   args: {
-    variant: 'transparent',
+    className: `${buttonStyles.base} ${buttonStyles.transparent}`,
   },
   render: args => <Button {...args}>Shy, But Clickable!</Button>,
 };
--- src/components/inputs/Button.tsx
+++ src/components/inputs/Button.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 import { Loading } from '../feedback';
 
+export const buttonStyles = {
+  base: 'px-4 py-2',
+  primary: 'font-bold bg-primary-600 dark:bg-primary-950 hover:bg-primary-700 text-white',
+  secondary: 'border border-gray-300 text-gray-700 hover:bg-primary-100',
+  light: 'bg-primary-100 text-gray-800 hover:bg-primary-200',
+  transparent: 'bg-transparent hover:bg-primary-200 hover:bg-opacity-20',
+};
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   ariaLabel?: string;
   children?: React.ReactNode;
@@ -9,37 +17,24 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   leftIcon?: React.ReactNode;
   className?: string;
   loading?: boolean;
-  lightText?: boolean; // New prop for light text
-  variant?: 'filled' | 'outline' | 'light' | 'transparent';
+  lightText?: boolean;
+  variant?: 'primary' | 'secondary' | 'light' | 'transparent';
 }
 
-const Button = (props: ButtonProps) => {
-  const { variant = 'filled', lightText = false } = props;
+const Button: React.FC<ButtonProps> = props => {
+  const { variant = 'primary', lightText = false } = props;
 
   const textColor = lightText ? 'text-white' : 'text-black';
-  let variantClass = '';
 
-  switch (variant) {
-    case 'transparent':
-      variantClass = `bg-transparent hover:bg-primary-200 hover:bg-opacity-20 ${textColor}`;
-      break;
-    case 'outline':
-      variantClass = 'border border-gray-300 text-gray-700 hover:bg-primary-100';
-      break;
-    case 'light':
-      variantClass = 'bg-primary-100 text-gray-800 hover:bg-primary-200';
-      break;
-    default:
-      variantClass = 'bg-primary-600 dark:bg-primary-950 hover:bg-primary-700 text-white';
-      break;
-  }
+  const buttonClassName =
+    props.className || `${buttonStyles.base} ${buttonStyles[variant]} ${textColor}`;
 
   return (
     <button
       disabled={props.disabled || props.loading}
       aria-label={props.ariaLabel}
       onClick={props.onClick}
-      className={`${variantClass} font-bold py-2 px-4 flex flex-row ${props.className ?? ''}`}
+      className={buttonClassName}
     >
       {props.leftIcon && <span className={`mr-2 ${textColor}`}>{props.leftIcon}</span>}
       <span className={textColor}>{props.children}</span>
