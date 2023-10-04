68192e28d565e04a294e00b3ba0d0cdb7f451f8d
Meinte <meinte@gmail.com>
2023-10-04 18:33:48 +0200

    fix: safely parse responses and catch and log errors (#140)
    * fix: safely parse responses and catch and log errors
    
    * fix: promisify safeParse error handling

--- src/utils/api/handler.ts
+++ src/utils/api/handler.ts
@@ -1,16 +1,29 @@
+import Logger from '../Logger';
 import ApiError from './ApiError';
 
 type StatusMap = {
   [status: number]: (response: Response) => Promise<any | string>;
 };
 
+const safeParse = async (response: Response, parseAsJson: boolean) => {
+  const logParseError = (error: Error) => {
+    Logger.error({ namespace: 'api' }, 'Trouble parsing result ', {
+      statusCode: response.status,
+      parseAsJson,
+      error,
+    });
+    return null;
+  };
+  if (parseAsJson) {
+    return response.json().catch(logParseError);
+  }
+
+  return response.text().catch(logParseError);
+};
+
 const statusMapper = {
-  200: (response: Response) => {
-    return response.json();
-  },
-  204: (response: Response) => {
-    return response.text();
-  },
+  200: (response: Response) => safeParse(response, true),
+  204: (response: Response) => safeParse(response, false),
   400: (response: Response) => {
     throw new ApiError({
       message: 'Api invalid request',
