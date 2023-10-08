5139206d98df97a77e6c288419fd01bf0d4c5eb1
Ole Kristian Losvik <losvik@gmail.com>
2023-10-08 19:59:13 +0200

    feat: add fields to api

--- src/Eventuras.WebApi/Controllers/Events/EventFormDto.cs
+++ src/Eventuras.WebApi/Controllers/Events/EventFormDto.cs
@@ -6,6 +6,10 @@ using NodaTime;
 
 namespace Eventuras.WebApi.Controllers.Events
 {
+    /// <summary>
+    /// Data Transfer Object (DTO) for Event Information.
+    /// Used for API between the backend and the frontend.
+    /// </summary>
     public class EventFormDto : IValidatableObject
     {
         [Required]
@@ -17,6 +21,8 @@ namespace Eventuras.WebApi.Controllers.Events
         public EventInfo.EventInfoType Type { get; set; } = EventInfo.EventInfoType.Course;
         public EventInfo.EventInfoStatus Status { get; set; } = EventInfo.EventInfoStatus.Draft;
         public int OrganizationId { get; set; }
+        public string Headline { get; set; }
+        public string MoreInformation { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
         public bool ManageRegistrations { get; set; }
@@ -28,8 +34,27 @@ namespace Eventuras.WebApi.Controllers.Events
         public string City { get; set; }
         public LocalDate? StartDate { get; set; }
         public LocalDate? EndDate { get; set; }
+
+        public string WelcomeLetter { get; set; }
+        public bool Published { get; set; }
+        public string ExternalInfoPageUrl { get; set; }
+        public string ExternalRegistrationsUrl { get; set; }
+        public string InformationRequest { get; set; }
+        public LocalDate? LastRegistrationDate { get; set; }
+        public LocalDate? LastCancellationDate { get; set; }
+        public int MaxParticipants { get; set; }
+        public string CertificateTitle { get; set; }
+        public string CertificateDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string FeaturedImageCaption { get; set; }
+        public string ProjectCode { get; set; }
+        public string OrganizerUserId { get; set; }
         public EventInfoOptionsDto Options { get; set; } = new(new());
 
+        /// <summary>
+        /// Copies the DTO properties into the EventInfo domain model.
+        /// </summary>
+        /// <param name="eventInfo">The EventInfo domain model to update.</param>
         public void CopyTo(EventInfo eventInfo)
         {
             if (eventInfo == null) { throw new ArgumentNullException(nameof(eventInfo)); }
@@ -50,15 +75,37 @@ namespace Eventuras.WebApi.Controllers.Events
             eventInfo.City = City;
             eventInfo.DateStart = StartDate;
             eventInfo.DateEnd = EndDate;
+            eventInfo.Headline = Headline;
+            eventInfo.MoreInformation = MoreInformation;
+            eventInfo.WelcomeLetter = WelcomeLetter;
+            eventInfo.Published = Published;
+            eventInfo.ExternalInfoPageUrl = ExternalInfoPageUrl;
+            eventInfo.ExternalRegistrationsUrl = ExternalRegistrationsUrl;
+            eventInfo.InformationRequest = InformationRequest;
+            eventInfo.LastRegistrationDate = LastRegistrationDate;
+            eventInfo.LastCancellationDate = LastCancellationDate;
+            eventInfo.MaxParticipants = MaxParticipants;
+            eventInfo.CertificateTitle = CertificateTitle;
+            eventInfo.CertificateDescription = CertificateDescription;
+            eventInfo.FeaturedImageUrl = FeaturedImageUrl;
+            eventInfo.FeaturedImageCaption = FeaturedImageCaption;
+            eventInfo.ProjectCode = ProjectCode;
+            eventInfo.OrganizerUserId = OrganizerUserId;
             eventInfo.Options = Options.MapToEntity();
         }
 
+        /// <summary>
+        /// Creates an EventFormDto object from an EventInfo entity.
+        /// </summary>
+        /// <param name="entity">The EventInfo entity to convert.</param>
+        /// <returns>An EventFormDto object containing the data from the EventInfo entity.</returns>
         public static EventFormDto FromEntity(EventInfo entity)
         {
             return new EventFormDto
             {
                 Title = entity.Title,
                 Slug = entity.Slug,
+                Headline = entity.Headline,
                 Type = entity.Type,
                 Status = entity.Status,
                 OrganizationId = entity.OrganizationId,
@@ -73,10 +120,30 @@ namespace Eventuras.WebApi.Controllers.Events
                 City = entity.City,
                 StartDate = entity.DateStart,
                 EndDate = entity.DateEnd,
+                MoreInformation = entity.MoreInformation,
+                WelcomeLetter = entity.WelcomeLetter,
+                Published = entity.Published,
+                ExternalInfoPageUrl = entity.ExternalInfoPageUrl,
+                ExternalRegistrationsUrl = entity.ExternalRegistrationsUrl,
+                InformationRequest = entity.InformationRequest,
+                LastRegistrationDate = entity.LastRegistrationDate,
+                LastCancellationDate = entity.LastCancellationDate,
+                MaxParticipants = entity.MaxParticipants,
+                CertificateTitle = entity.CertificateTitle,
+                CertificateDescription = entity.CertificateDescription,
+                FeaturedImageUrl = entity.FeaturedImageUrl,
+                FeaturedImageCaption = entity.FeaturedImageCaption,
+                ProjectCode = entity.ProjectCode,
+                OrganizerUserId = entity.OrganizerUserId,
                 Options = EventInfoOptionsDto.MapFromEntity(entity.Options),
             };
         }
 
+        /// <summary>
+        /// Validates the EventFormDto object.
+        /// </summary>
+        /// <param name="validationContext">The context in which to validate the object.</param>
+        /// <returns>A list of ValidationResults if validation fails, otherwise an empty list.</returns>
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if (StartDate.HasValue && EndDate.HasValue && StartDate.Value > EndDate.Value)
