6d89c8711720aca1f3c2a16377c5e2ec35a0e49e
Ole Kristian Losvik <losvik@gmail.com>
2023-10-19 21:21:57 +0200

    feat: remove deprecated projects

--- src/Eventuras.Services.TalentLms/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Eventuras.Services.TalentLms.Tests")]
-namespace Eventuras.Services.TalentLms.Properties
-{
-    internal class AssemblyInfo
-    {
-    }
-}
--- src/Eventuras.Web/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace Eventuras.Web
-{
-    public static class Constants
-    {
-        public const string HealthCheckName = "Event Management System Checks";
-        public const string HealthCheckUri = "/health";
-        public const string HealthCheckConfigurationKey = "HealthChecks";
-    }
-}
