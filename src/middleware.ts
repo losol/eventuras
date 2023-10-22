ddf546de9ccad3d5b58415ebd70c6e4fd145664b
Ole Kristian Losvik <losvik@gmail.com>
2023-10-22 19:14:51 +0200

    feat(auth): adds token as a header
    Makes it possible for Next server pages to access the token. Server pages does not seem to have access to the request object, but could read headers. With this change, bearer token could be accessed by `headers().get('Authorization')?.substring(7)`

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,35 @@
  * This saves individual pages to have to check for authorization.
  */
 
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 import { withAuth } from 'next-auth/middleware';
+
+import Logger from './utils/Logger';
+
+export async function middleware(request: NextRequest) {
+  try {
+    const token = await getToken({ req: request });
+    const requestHeaders = new Headers(request.headers);
+
+    // If the token exists, set it as an Authorization header
+    if (token?.access_token) {
+      requestHeaders.set('Authorization', `Bearer ${token.access_token}`);
+    } else {
+      return NextResponse.redirect('/api/auth/signin');
+    }
+
+    // Return the original request with the new headers
+    return NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    });
+  } catch (error) {
+    Logger.error({ namespace: 'auth' }, 'Error in middleware: ' + error);
+  }
+}
+
 export default withAuth({
   pages: {
     signIn: '/api/auth/signin',
@@ -13,4 +41,5 @@ export default withAuth({
     error: '/api/auth/error',
   },
 });
+
 export const config = { matcher: ['/admin/:path*', '/user/:path*'] };
