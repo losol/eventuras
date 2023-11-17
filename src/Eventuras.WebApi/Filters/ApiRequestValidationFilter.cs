de930d4a185994627a2ff143e12e3706a1737380
Ole Kristian Losvik <losvik@gmail.com>
2023-11-17 23:06:29 +0100

    feat: add more useful errors on bad requests

---
+++ src/Eventuras.WebApi/Filters/ApiRequestValidationFilter.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Logging;
+
+namespace Eventuras.WebApi.Filters;
+
+public class ValidationFilter : IAsyncActionFilter
+{
+    private readonly ProblemDetailsFactory _problemDetailsFactory;
+    private readonly ILogger<ValidationFilter> _logger;
+
+    public ValidationFilter(ProblemDetailsFactory problemDetailsFactory, ILogger<ValidationFilter> logger)
+    {
+        _problemDetailsFactory = problemDetailsFactory;
+        _logger = logger;
+    }
+
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        if (!context.ModelState.IsValid)
+        {
+            var errors = context.ModelState
+                .Where(x => x.Value.Errors.Any())
+                .SelectMany(x => x.Value.Errors.Select(e => new { Field = x.Key, Error = e.ErrorMessage }))
+                .ToList();
+
+            var problemDetails = _problemDetailsFactory.CreateValidationProblemDetails(
+                context.HttpContext,
+                context.ModelState,
+                statusCode: StatusCodes.Status400BadRequest,
+                title: "Validation Error",
+                detail: "One or more validation errors occurred."
+            );
+
+            problemDetails.Extensions["errors"] = errors;
+
+            _logger.LogWarning("Validation errors occurred: {Errors}", errors);
+
+            context.Result = new BadRequestObjectResult(problemDetails);
+            return;
+        }
+
+        await next();
+    }
+}
+
