6663841ebeb0959fdd44da90f6ca2c2cdf92d62d
Ole Kristian Losvik <losvik@gmail.com>
2024-01-08 23:40:29 +0100

    feat: render event editing page server side

--- src/app/admin/events/[id]/edit/page.tsx
+++ src/app/admin/events/[id]/edit/page.tsx
@@ -1,11 +1,11 @@
-'use client';
+import { headers } from 'next/headers';
 import createTranslation from 'next-translate/createTranslation';
 
 import EventEditor from '@/app/admin/events/EventEditor';
 import { Layout } from '@/components/ui';
 import Heading from '@/components/ui/Heading';
-import useCreateHook from '@/hooks/createHook';
-import { createSDK } from '@/utils/api/EventurasApi';
+import { apiWrapper, createSDK } from '@/utils/api/EventurasApi';
+import Environment from '@/utils/Environment';
 
 type EditEventinfoProps = {
   params: {
@@ -13,23 +13,29 @@ type EditEventinfoProps = {
   };
 };
 
-const EditEventinfo: React.FC<EditEventinfoProps> = ({ params }) => {
-  const eventId = parseInt(params.id, 10);
-  const { result, loading } = useCreateHook(
-    () => createSDK().events.getV3Events1({ id: eventId }),
-    [eventId]
-  );
+const EditEventinfo: React.FC<EditEventinfoProps> = async ({ params }) => {
+  const eventId = parseInt(params.id);
   const { t } = createTranslation();
 
-  if (loading) return null;
-  if (!result) {
+  const eventuras = createSDK({
+    baseUrl: Environment.NEXT_PUBLIC_BACKEND_URL,
+    authHeader: headers().get('Authorization'),
+  });
+
+  const eventinfo = await apiWrapper(() =>
+    eventuras.events.getV3Events1({
+      id: eventId,
+    })
+  );
+
+  if (!eventinfo.ok) {
     return <div>{t('common:event-not-found')}</div>;
   }
+
   return (
     <Layout>
       <Heading>{t(`admin:editEvent.content.title`)}</Heading>
-      <p>{t(`admin:editEvent.content.description`)}</p>
-      <EventEditor eventinfo={result} />
+      <EventEditor eventinfo={eventinfo.value!} />
     </Layout>
   );
 };
