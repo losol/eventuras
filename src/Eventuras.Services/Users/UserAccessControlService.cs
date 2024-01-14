97d8df60ea5ebd83078fab6ee68f6c6b34d1208a
Ole Kristian Losvik <losvik@gmail.com>
2024-01-14 23:02:49 +0100

    chore: updates for 2024-01-14

--- src/Eventuras.Services/Users/UserAccessControlService.cs
+++ src/Eventuras.Services/Users/UserAccessControlService.cs
@@ -16,27 +16,21 @@ namespace Eventuras.Services.Users
     {
 
         private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly IUserRetrievalService _userRetrievalService;
-        private readonly ICurrentOrganizationAccessorService _currentOrganizationAccessorService;
         private readonly ILogger<UserAccessControlService> _logger;
 
         public UserAccessControlService(
             IHttpContextAccessor httpContextAccessor,
-            IUserRetrievalService userRetrievalService,
-            ICurrentOrganizationAccessorService currentOrganizationAccessorService,
             ILogger<UserAccessControlService> logger
             )
         {
             _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
-            _userRetrievalService = userRetrievalService ?? throw new ArgumentNullException(nameof(userRetrievalService));
-            _currentOrganizationAccessorService = currentOrganizationAccessorService ?? throw new ArgumentNullException(nameof(currentOrganizationAccessorService));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
 
         public async Task CheckOwnerOrAdminAccessAsync(ApplicationUser user, CancellationToken cancellationToken)
         {
-            var requestingUser = _httpContextAccessor.HttpContext!.User;
+            var requestingUser = _httpContextAccessor.HttpContext.User;
 
             // Log information about the access check
             _logger.LogInformation($"Checking owner or admin access for user {requestingUser.GetUserId} for editing user {user.Id}");
@@ -47,24 +41,29 @@ namespace Eventuras.Services.Users
             }
 
             // Check if the requesting user is the owner
-            if (user.Id == requestingUser.GetUserId().ToString())
+            if (user.Id == requestingUser.GetUserId())
             {
                 _logger.LogInformation("Owner access granted.");
                 return;
             }
 
             // Check if the requesting user is an admin
-            await CheckAdminAccessAsync(requestingUser, user, cancellationToken);
+            var isAdmin = CheckAdminAccessAsync(requestingUser);
+
+            if (isAdmin)
+            {
+                _logger.LogInformation("Admin access granted.");
+                return;
+            }
 
             // If neither, throw access denied exception
             throw new NotAccessibleException("Access denied. User is neither owner nor admin.");
         }
 
-        private async Task<bool> CheckAdminAccessAsync(ClaimsPrincipal requestingUser, ApplicationUser user, CancellationToken cancellationToken)
+        private bool CheckAdminAccessAsync(ClaimsPrincipal user)
         {
-            if (requestingUser.IsPowerAdmin()) { return true; }
-
-            if (requestingUser.IsAdmin()) { return true; }
+            if (user.IsAdmin()) { return true; }
+            if (user.IsPowerAdmin()) { return true; }
 
             return false;
         }
--- tests/Eventuras.TestAbstractions/TestingConstants.cs
+++ tests/Eventuras.TestAbstractions/TestingConstants.cs
@@ -7,15 +7,6 @@ namespace Eventuras.TestAbstractions
         public const string SuperAdminEmail = "super-admin@email.com";
         public const string SuperAdminPassword = "MySuperSecretKey1!";
 
-        public static readonly string[] DefaultScopes = {
-            "events:read",
-            "events:write",
-            "registrations:read",
-            "registrations:write",
-            "users:read",
-            "users:write"
-        };
-
         public const string DefaultPassword = "MySuperSecretPassword1!";
     }
 }
