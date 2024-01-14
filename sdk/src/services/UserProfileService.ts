0b50cc737388f2edc85326d222bfda4b2dee311f
Ole Kristian Losvik <losvik@gmail.com>
2024-01-14 20:38:07 +0100

    feat(sdk): release sdk v0.6.5

---
+++ sdk/src/services/UserProfileService.ts
@@ -0,0 +1,61 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { UserDto } from '../models/UserDto';
+import type { UserFormDto } from '../models/UserFormDto';
+import type { CancelablePromise } from '../core/CancelablePromise';
+import type { BaseHttpRequest } from '../core/BaseHttpRequest';
+export class UserProfileService {
+    constructor(public readonly httpRequest: BaseHttpRequest) {}
+    /**
+     * Gets information about the current user. Creates a new user if no user with the email exists.
+     * @returns UserDto Success
+     * @throws ApiError
+     */
+    public getV3Userprofile({
+        eventurasOrgId,
+    }: {
+        /**
+         * Optional organization Id. Will be required in API version 4.
+         */
+        eventurasOrgId?: number,
+    }): CancelablePromise<UserDto> {
+        return this.httpRequest.request({
+            method: 'GET',
+            url: '/v3/userprofile',
+            headers: {
+                'Eventuras-Org-Id': eventurasOrgId,
+            },
+        });
+    }
+    /**
+     * @returns UserDto Success
+     * @throws ApiError
+     */
+    public putV3Userprofile({
+        id,
+        eventurasOrgId,
+        requestBody,
+    }: {
+        id?: string,
+        /**
+         * Optional organization Id. Will be required in API version 4.
+         */
+        eventurasOrgId?: number,
+        requestBody?: UserFormDto,
+    }): CancelablePromise<UserDto> {
+        return this.httpRequest.request({
+            method: 'PUT',
+            url: '/v3/userprofile',
+            headers: {
+                'Eventuras-Org-Id': eventurasOrgId,
+            },
+            query: {
+                'id': id,
+            },
+            body: requestBody,
+            mediaType: 'application/json',
+        });
+    }
+}
