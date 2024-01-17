9111bb74e40fe93d0f3bee011f2832691c5105ce
Ole Kristian Losvik <losvik@gmail.com>
2024-01-17 22:38:21 +0100

    feat: add registration status to delivery report

--- sdk/src/models/ProductOrdersSummaryDto.ts
+++ sdk/src/models/ProductOrdersSummaryDto.ts
@@ -2,9 +2,11 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { RegistrationStatus } from './RegistrationStatus';
 import type { UserSummaryDto } from './UserSummaryDto';
 export type ProductOrdersSummaryDto = {
     registrationId?: number;
+    registrationStatus?: RegistrationStatus;
     user?: UserSummaryDto;
     orderIds?: Array<number> | null;
     sumQuantity?: number;
