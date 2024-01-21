2eb652fcbaa26d878f3eb5789f7a04059dbc0555
Ole Kristian Losvik <losvik@gmail.com>
2024-01-21 10:21:28 +0100

    refactor: move statistics logic to service layer * also requires admin role to access

--- src/Eventuras.Services/Registrations/EventRegistrationStatistics.cs
+++ src/Eventuras.Services/Registrations/EventRegistrationStatistics.cs
@@ -3,8 +3,28 @@ using Eventuras.Domain;
 
 namespace Eventuras.Servcies.Registrations;
 
+
 public class RegistrationStatistics
 {
-    public Dictionary<Registration.RegistrationStatus, int> StatusCounts { get; set; }
-    public Dictionary<Registration.RegistrationType, int> TypeCounts { get; set; }
+    public ByStatus ByStatus { get; set; }
+    public ByType ByType { get; set; }
+}
+public class ByStatus
+{
+    public int Draft { get; set; }
+    public int Cancelled { get; set; }
+    public int Verified { get; set; }
+    public int NotAttended { get; set; }
+    public int Attended { get; set; }
+    public int Finished { get; set; }
+    public int WaitingList { get; set; }
+}
+
+public class ByType
+{
+    public int Participant { get; set; }
+    public int Student { get; set; }
+    public int Staff { get; set; }
+    public int Lecturer { get; set; }
+    public int Artist { get; set; }
 }
--- src/Eventuras.WebApi/Controllers/v3/Events/Statistics/EventStatisticsController.cs
+++ src/Eventuras.WebApi/Controllers/v3/Events/Statistics/EventStatisticsController.cs
@@ -1,5 +1,6 @@
 using Asp.Versioning;
 using Eventuras.Domain;
+using Eventuras.Services;
 using Eventuras.Services.Auth;
 using Eventuras.Services.Registrations;
 using Eventuras.WebApi.Models;
@@ -16,7 +17,7 @@ using System.Threading.Tasks;
 namespace Eventuras.WebApi.Controllers.v3.Events.Statistics
 {
     [ApiVersion("3")]
-    [Authorize]
+    [Authorize(Roles = Roles.Admin)]
     [Route("v{version:apiVersion}/events/{eventId}/")]
     [ApiController]
     public class EventStatisticsController : ControllerBase
@@ -39,35 +40,7 @@ namespace Eventuras.WebApi.Controllers.v3.Events.Statistics
         public async Task<ActionResult<EventStatisticsDto>> GetEventStatistics(int eventId, CancellationToken cancellationToken)
         {
             var registrationStatistics = await _registrationRetrievalService.GetRegistrationStatisticsAsync(eventId, cancellationToken);
-
-            var byStatus = new ByStatus
-            {
-                Draft = registrationStatistics.StatusCounts.TryGetValue(Registration.RegistrationStatus.Draft, out var draftCount) ? draftCount : 0,
-                Cancelled = registrationStatistics.StatusCounts.TryGetValue(Registration.RegistrationStatus.Cancelled, out var cancelledCount) ? cancelledCount : 0,
-                Verified = registrationStatistics.StatusCounts.TryGetValue(Registration.RegistrationStatus.Verified, out var verifiedCount) ? verifiedCount : 0,
-                NotAttended = registrationStatistics.StatusCounts.TryGetValue(Registration.RegistrationStatus.NotAttended, out var notAttendedCount) ? notAttendedCount : 0,
-                Attended = registrationStatistics.StatusCounts.TryGetValue(Registration.RegistrationStatus.Attended, out var attendedCount) ? attendedCount : 0,
-                Finished = registrationStatistics.StatusCounts.TryGetValue(Registration.RegistrationStatus.Finished, out var finishedCount) ? finishedCount : 0,
-                WaitingList = registrationStatistics.StatusCounts.TryGetValue(Registration.RegistrationStatus.WaitingList, out var waitingListCount) ? waitingListCount : 0
-            };
-
-            var byType = new ByType
-            {
-                Participant = registrationStatistics.TypeCounts.TryGetValue(Registration.RegistrationType.Participant, out var participantCount) ? participantCount : 0,
-                Student = registrationStatistics.TypeCounts.TryGetValue(Registration.RegistrationType.Student, out var studentCount) ? studentCount : 0,
-                Staff = registrationStatistics.TypeCounts.TryGetValue(Registration.RegistrationType.Staff, out var staffCount) ? staffCount : 0,
-                Lecturer = registrationStatistics.TypeCounts.TryGetValue(Registration.RegistrationType.Lecturer, out var lecturerCount) ? lecturerCount : 0,
-                Artist = registrationStatistics.TypeCounts.TryGetValue(Registration.RegistrationType.Artist, out var artistCount) ? artistCount : 0
-            };
-
-            var statistics = new EventStatisticsDto
-            {
-                ByStatus = byStatus,
-                ByType = byType
-            };
-
-            return Ok(statistics);
+            return Ok(registrationStatistics);
         }
-
     }
 }
