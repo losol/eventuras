f5d0308f864d2d8d0d9cad7d35904be4818275a8
Ole Kristian Losvik <losvik@gmail.com>
2024-02-03 21:41:12 +0100

    fix: sort imports

--- apps/web/src/app/events/EventRegistrationButton.tsx
+++ apps/web/src/app/events/EventRegistrationButton.tsx
@@ -1,23 +1,25 @@
+import { EventDto, EventInfoStatus } from '@losol/eventuras';
 import createTranslation from 'next-translate/createTranslation';
 
 import Link from '@/components/ui/Link';
-import { EventDto, EventInfoStatus } from '@losol/eventuras';
 
 export type EventRegistrationButtonProps = {
-  event: EventDto
+  event: EventDto;
 };
 
 export default function EventRegistrationButton({ event }: EventRegistrationButtonProps) {
   const { t } = createTranslation();
-  const canRegister = event.status === EventInfoStatus.REGISTRATIONS_OPEN
-  const link = <Link
-    href={`/user/events/${event.id}`}
-    variant="button-primary"
-    block
-    data-test-id="event-registration-button"
-  >
-    {t('common:buttons.register-cta')}
-  </Link>
+  const canRegister = event.status === EventInfoStatus.REGISTRATIONS_OPEN;
+  const link = (
+    <Link
+      href={`/user/events/${event.id}`}
+      variant="button-primary"
+      block
+      data-test-id="event-registration-button"
+    >
+      {t('common:buttons.register-cta')}
+    </Link>
+  );
 
-  return canRegister ? link : <span>{t('common:buttons.registration-not-open')}</span>
+  return canRegister ? link : <span>{t('common:buttons.registration-not-open')}</span>;
 }
