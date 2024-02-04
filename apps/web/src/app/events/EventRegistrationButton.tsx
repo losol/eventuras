cac4b4427177803df8a238e67510f5f8791c72e1
Ole Kristian Losvik <losvik@gmail.com>
2024-02-04 19:05:38 +0100

    feat: show badge or registration button depending on event status

--- apps/web/src/app/events/EventRegistrationButton.tsx
+++ apps/web/src/app/events/EventRegistrationButton.tsx
@@ -1,6 +1,7 @@
 import { EventDto, EventInfoStatus } from '@losol/eventuras';
 import createTranslation from 'next-translate/createTranslation';
 
+import Badge from '@/components/ui/Badge';
 import Link from '@/components/ui/Link';
 
 export type EventRegistrationButtonProps = {
@@ -10,16 +11,42 @@ export type EventRegistrationButtonProps = {
 export default function EventRegistrationButton({ event }: EventRegistrationButtonProps) {
   const { t } = createTranslation();
   const canRegister = event.status === EventInfoStatus.REGISTRATIONS_OPEN;
-  const link = (
-    <Link
-      href={`/user/events/${event.id}`}
-      variant="button-primary"
-      block
-      data-test-id="event-registration-button"
-    >
-      {t('common:buttons.register-cta')}
-    </Link>
-  );
 
-  return canRegister ? link : <span>{t('common:buttons.registration-not-open')}</span>;
+  const getStatusText = (status: EventInfoStatus): string => {
+    switch (status) {
+      case EventInfoStatus.DRAFT:
+        return t('common:events.labels.status.draft');
+      case EventInfoStatus.PLANNED:
+        return t('common:events.labels.status.planned');
+      case EventInfoStatus.REGISTRATIONS_OPEN:
+        return t('common:events.labels.status.registrationsOpen');
+      case EventInfoStatus.WAITING_LIST:
+        return t('common:events.labels.status.waitingList');
+      case EventInfoStatus.REGISTRATIONS_CLOSED:
+        return t('common:events.labels.status.registrationsClosed');
+      case EventInfoStatus.FINISHED:
+        return t('common:events.labels.status.finished');
+      case EventInfoStatus.ARCHIVED:
+        return t('common:events.labels.status.archived');
+      case EventInfoStatus.CANCELLED:
+        return t('common:events.labels.status.cancelled');
+      default:
+        return t('common:events.labels.status.unknown');
+    }
+  };
+
+  if (canRegister) {
+    return (
+      <Link
+        href={`/user/events/${event.id}`}
+        variant="button-primary"
+        block
+        data-test-id="event-registration-button"
+      >
+        {t('common:buttons.register-cta')}
+      </Link>
+    );
+  } else {
+    return <Badge block>{getStatusText(event.status!)}</Badge>;
+  }
 }
--- apps/web/src/components/ui/Badge.tsx
+++ apps/web/src/components/ui/Badge.tsx
@@ -4,9 +4,15 @@ type BadgeProps = {
   children: React.ReactNode;
   className?: string;
   variant?: 'neutral' | 'info' | 'positive' | 'negative';
+  block?: boolean;
 };
 
-const Badge: React.FC<BadgeProps> = ({ children, className = '', variant = 'neutral' }) => {
+const Badge: React.FC<BadgeProps> = ({
+  children,
+  className = '',
+  variant = 'neutral',
+  block = false,
+}) => {
   const variantClasses = {
     neutral: 'bg-gray-500 dark:bg-gray-700',
     info: 'bg-blue-500 dark:bg-blue-700',
@@ -14,9 +20,11 @@ const Badge: React.FC<BadgeProps> = ({ children, className = '', variant = 'neut
     negative: 'bg-red-500 dark:bg-red-700',
   };
 
+  const blockClass = block ? 'block py-2 my-2' : '';
+
   return (
     <span
-      className={`inline-block px-2 py-1 text-xs font-semibold leading-none text-white rounded ${variantClasses[variant]} ${className}`}
+      className={`${blockClass} px-2 py-1 text-xs font-semibold leading-none text-white rounded ${variantClasses[variant]} ${className}`}
     >
       {children}
     </span>
