490a9398d27d09e09ea2ca601ade85cc7a344e5b
Ole Kristian Losvik <losvik@gmail.com>
2024-02-09 00:07:31 +0100

    fix: products where not updated from put

--- apps/api/src/Eventuras.WebApi/Controllers/v3/Events/Products/ProductFormDto.cs
+++ apps/api/src/Eventuras.WebApi/Controllers/v3/Events/Products/ProductFormDto.cs
@@ -5,16 +5,26 @@ namespace Eventuras.WebApi.Controllers.v3.Events.Products
 {
     public class ProductFormDto
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal VatPercent { get; set; }
+
         public bool? Published { get; set; }
 
         public ProductVisibility? Visibility { get; set; }
 
-        public void ToProduct(Product product)
+        public void CopyTo(Product product)
         {
             if (product == null)
             {
                 throw new ArgumentNullException(nameof(product));
             }
+
+            Name = product.Name;
+            Description = product.Description;
+            Price = product.Price;
+            VatPercent = product.VatPercent;
 
             if (Published.HasValue)
             {
