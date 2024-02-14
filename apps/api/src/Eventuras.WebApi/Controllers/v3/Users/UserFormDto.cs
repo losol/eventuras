dab7c2dde7d99301a84ab935dcf53fbf3609dca8
Ole Kristian Losvik <losvik@gmail.com>
2024-02-14 21:03:26 +0100

    feat: extend userprofil api dtos

--- apps/api/src/Eventuras.WebApi/Controllers/v3/Users/UserFormDto.cs
+++ apps/api/src/Eventuras.WebApi/Controllers/v3/Users/UserFormDto.cs
@@ -1,11 +1,12 @@
 using Eventuras.Domain;
+using System;
 using System.ComponentModel.DataAnnotations;
+using NodaTime;
 
 namespace Eventuras.WebApi.Controllers.v3.Users
 {
     public class UserFormDto
     {
-        [Required]
         public string Name { get; set; }
 
         [Required]
@@ -15,11 +16,65 @@ namespace Eventuras.WebApi.Controllers.v3.Users
         [Phone]
         public string PhoneNumber { get; set; }
 
+        // Personal information fields
+        public string GivenName { get; set; }
+        public string MiddleName { get; set; }
+        public string FamilyName { get; set; }
+        public bool NameVerified { get; set; }
+
+        // Profile picture URL
+        public string PictureUrl { get; set; }
+
+        // Address information
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        // Birthdate and verification
+        public LocalDate? BirthDate { get; set; } // Ensure LocalDate is correctly handled or use DateTime
+        public bool BirthDateVerified { get; set; }
+
+        // Professional information
+        public string Profession { get; set; }
+        public string JobRole { get; set; }
+        public string Employer { get; set; }
+        public string EmployerIdentificationNumber { get; set; }
+        public string ProfessionalIdentityNumber { get; set; }
+        public bool ProfessionalIdentityNumberVerified { get; set; }
+
+        // Additional user information
+        public string SupplementaryInformation { get; set; }
+        public bool Archived { get; set; }
+
         public void CopyTo(ApplicationUser user)
         {
             user.Name = Name;
             user.Email = Email;
             user.PhoneNumber = PhoneNumber;
+
+            // Copying the additional fields
+            user.GivenName = GivenName;
+            user.MiddleName = MiddleName;
+            user.FamilyName = FamilyName;
+            user.NameVerified = NameVerified;
+            user.PictureUrl = PictureUrl;
+            user.AddressLine1 = AddressLine1;
+            user.AddressLine2 = AddressLine2;
+            user.ZipCode = ZipCode;
+            user.City = City;
+            user.Country = Country;
+            user.BirthDate = BirthDate;
+            user.BirthDateVerified = BirthDateVerified;
+            user.Profession = Profession;
+            user.JobRole = JobRole;
+            user.Employer = Employer;
+            user.EmployerIdentificationNumber = EmployerIdentificationNumber;
+            user.ProfessionalIdentityNumber = ProfessionalIdentityNumber;
+            user.ProfessionalIdentityNumberVerified = ProfessionalIdentityNumberVerified;
+            user.SupplementaryInformation = SupplementaryInformation;
+            user.Archived = Archived;
         }
     }
 }
