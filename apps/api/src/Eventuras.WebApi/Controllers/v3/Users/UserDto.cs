5580f41c808c02f80552319a389ec245dec2e95f
Ole Kristian Losvik <losvik@gmail.com>
2024-02-14 21:03:26 +0100

    feat: extend userprofil api dtos

--- apps/api/src/Eventuras.WebApi/Controllers/v3/Users/UserDto.cs
+++ apps/api/src/Eventuras.WebApi/Controllers/v3/Users/UserDto.cs
@@ -1,20 +1,48 @@
 using Eventuras.Domain;
+using NodaTime;
 
 namespace Eventuras.WebApi.Controllers.v3.Users
 {
     public class UserDto
     {
         public string Id { get; set; }
-
         public string Name { get; set; }
-
         public string Email { get; set; }
-
         public string PhoneNumber { get; set; }
 
-        public UserDto()
-        {
-        }
+        // Personal information fields
+        public string GivenName { get; set; }
+        public string MiddleName { get; set; }
+        public string FamilyName { get; set; }
+        public bool NameVerified { get; set; }
+
+        // Profile picture URL
+        public string PictureUrl { get; set; }
+
+        // Address information
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        // Birthdate and verification
+        public LocalDate? BirthDate { get; set; }
+        public bool BirthDateVerified { get; set; }
+
+        // Professional information
+        public string Profession { get; set; }
+        public string JobRole { get; set; }
+        public string Employer { get; set; }
+        public string EmployerIdentificationNumber { get; set; }
+        public string ProfessionalIdentityNumber { get; set; }
+        public bool ProfessionalIdentityNumberVerified { get; set; }
+
+        // Additional user information
+        public string SupplementaryInformation { get; set; }
+        public bool Archived { get; set; }
+
+        public UserDto() { }
 
         public UserDto(ApplicationUser user)
         {
@@ -22,6 +50,28 @@ namespace Eventuras.WebApi.Controllers.v3.Users
             Name = user.Name;
             Email = user.Email;
             PhoneNumber = user.PhoneNumber;
+
+            // Mapping the additional fields
+            GivenName = user.GivenName;
+            MiddleName = user.MiddleName;
+            FamilyName = user.FamilyName;
+            NameVerified = user.NameVerified;
+            PictureUrl = user.PictureUrl;
+            AddressLine1 = user.AddressLine1;
+            AddressLine2 = user.AddressLine2;
+            ZipCode = user.ZipCode;
+            City = user.City;
+            Country = user.Country;
+            BirthDate = user.BirthDate;
+            BirthDateVerified = user.BirthDateVerified;
+            Profession = user.Profession;
+            JobRole = user.JobRole;
+            Employer = user.Employer;
+            EmployerIdentificationNumber = user.EmployerIdentificationNumber;
+            ProfessionalIdentityNumber = user.ProfessionalIdentityNumber;
+            ProfessionalIdentityNumberVerified = user.ProfessionalIdentityNumberVerified;
+            SupplementaryInformation = user.SupplementaryInformation;
+            Archived = user.Archived;
         }
     }
 }
