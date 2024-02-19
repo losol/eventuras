5e9d3de4d671ee2c423faa4ef495e3ea2d2f4a92
Ole Kristian Losvik <losvik@gmail.com>
2024-02-19 19:19:50 +0100

    feat: add docs to docsite

--- apps/docsite/theme.config.jsx
+++ apps/docsite/theme.config.jsx
@@ -1,6 +1,10 @@
 export default {
+  footer: {
+    text: `GPL2+. ${new Date().getFullYear()} © Eventuras.`,
+    },
   logo: <span>Eventuras Documentation</span>,
   project: {
     link: 'https://github.com/losol/eventuras'
-  }
+  },
+  docsRepositoryBase: 'https://github.com/losol/eventuras/tree/main/docs'
 }
