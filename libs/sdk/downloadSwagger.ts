4018101e24ba92029fa5914e058848c8b0cc5f62
Ole Kristian Losvik <losvik@gmail.com>
2024-02-24 15:54:45 +0100

    ci: graceful failing when build without backend running

--- libs/sdk/downloadSwagger.ts
+++ libs/sdk/downloadSwagger.ts
@@ -11,15 +11,17 @@ const SWAGGER_URL = process.env.SWAGGER_URL || 'http://localhost:8080/swagger/v3
 const OUTPUT_PATH = join(__dirname, 'swagger.json');
 
 const downloadFile = async (url: string, outputPath: string): Promise<void> => {
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`Failed to download: ${response.statusText}`);
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Could not download swagger definition: ${response.statusText}`);
+        }
+        const data = await response.text();
+        await fs.writeFile(outputPath, data);
+        console.log(`Swagger JSON has been downloaded and saved to ${outputPath}`);
+    } catch (error) {
+        console.warn('Warning: Could not download swagger definition.');
     }
-    const data = await response.text();
-    await fs.writeFile(outputPath, data);
-    console.log(`Swagger JSON has been downloaded and saved to ${outputPath}`);
 };
 
-downloadFile(SWAGGER_URL, OUTPUT_PATH).catch(error => {
-    console.error('Error:', error);
-});
+downloadFile(SWAGGER_URL, OUTPUT_PATH);
