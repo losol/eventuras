813d3b0b2a24e7f4f09bd750aeaa44a87120e36d
Ole Kristian Losvik <losvik@gmail.com>
2024-03-17 21:02:23 +0100

    test: improve (?) garbage control in tests

--- apps/api/tests/Eventuras.TestAbstractions/DisposableEntity.cs
+++ apps/api/tests/Eventuras.TestAbstractions/DisposableEntity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Eventuras.Domain;
 using Microsoft.AspNetCore.Identity;
@@ -12,75 +13,133 @@ namespace Eventuras.TestAbstractions;
 /// events, registrations, and other.
 /// </summary>
 /// <typeparam name="T">Entity type</typeparam>
-public interface IDisposableEntity<out T> : IDisposable where T : class
+public interface IDisposableEntity<out T> : IDisposable, IAsyncDisposable where T : class
 {
-    public T Entity { get; }
-
-    public Task SaveAsync();
+    T Entity { get; }
+    Task SaveAsync();
 }
 
 public class DisposableEntity<T> : IDisposableEntity<T> where T : class
 {
     public T Entity { get; }
-
     private readonly DbContext _context;
-
     private readonly IDisposable[] _disposables;
+    private bool _disposed = false; // To detect redundant calls
 
     public DisposableEntity(T entity, DbContext context, params IDisposable[] disposables)
     {
-        Entity = entity;
-        _context = context;
+        Entity = entity ?? throw new ArgumentNullException(nameof(entity));
+        _context = context ?? throw new ArgumentNullException(nameof(context));
         _disposables = disposables;
-    }
-
-    public void Dispose()
-    {
-        foreach (var disposable in _disposables)
-        {
-            disposable.Dispose();
-        }
-
-        try
-        {
-            _context.Remove(Entity);
-            _context.SaveChanges();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-            //  Attempted to update or delete an entity that does not exist in the store.
-            _context.Entry(Entity).State = EntityState.Detached;
-        }
     }
 
     public async Task SaveAsync()
     {
         await _context.SaveChangesAsync();
     }
+
+    // Implement IDisposable.
+    public void Dispose()
+    {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await DisposeAsyncCore();
+
+        Dispose(disposing: false); // Prevent double dispose.
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            if (disposing)
+            {
+                // Dispose managed state (managed objects).
+                foreach (var disposable in _disposables)
+                {
+                    disposable.Dispose();
+                }
+            }
+
+            _disposed = true;
+        }
+    }
+
+    protected virtual async ValueTask DisposeAsyncCore()
+    {
+        // Async dispose managed state (managed objects) if needed.
+        foreach (var disposable in _disposables.OfType<IAsyncDisposable>())
+        {
+            await disposable.DisposeAsync();
+        }
+
+        try
+        {
+            _context.Remove(Entity);
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            // Attempted to update or delete an entity that does not exist in the store.
+            _context.Entry(Entity).State = EntityState.Detached;
+        }
+    }
 }
 
 public class DisposableUser : IDisposableEntity<ApplicationUser>
 {
-    public ApplicationUser Entity { get; }
-
+    private ApplicationUser _entity;
     private readonly UserManager<ApplicationUser> _userManager;
+    private bool _disposed = false;
 
-    public DisposableUser(
-        ApplicationUser entity,
-        UserManager<ApplicationUser> userManager)
+    public DisposableUser(ApplicationUser entity, UserManager<ApplicationUser> userManager)
     {
-        Entity = entity;
-        _userManager = userManager;
+        _entity = entity ?? throw new ArgumentNullException(nameof(entity));
+        _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+    }
+
+    public ApplicationUser Entity => _entity;
+
+    public Task SaveAsync()
+    {
+        return Task.CompletedTask;
     }
 
     public void Dispose()
     {
-        _userManager.DeleteAsync(Entity).Wait();
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 
-    public Task SaveAsync()
+    public async ValueTask DisposeAsync()
     {
-        // Stub
-        return Task.CompletedTask;
+        await DisposeAsyncCore();
+
+        // Perform traditional dispose operations, too, but indicate we're disposing asynchronously.
+        Dispose(disposing: false);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_disposed)
+        {
+            _entity = null;
+            _disposed = true;
+        }
+    }
+
+    protected virtual async ValueTask DisposeAsyncCore()
+    {
+        if (_entity != null)
+        {
+            await _userManager.DeleteAsync(_entity);
+            _entity = null;
+        }
     }
 }
