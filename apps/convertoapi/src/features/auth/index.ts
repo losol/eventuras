36e36c1b30614bac48769cefc9c673890226b421
Ole Kristian Losvik <losvik@gmail.com>
2024-03-27 22:55:11 +0100

    feat(converto): add docker support

--- apps/convertoapi/src/features/auth/index.ts
+++ apps/convertoapi/src/features/auth/index.ts
@@ -4,7 +4,7 @@ import authPlugin from './authPlugin';
 
 export async function registerAuthPlugin(fastify: FastifyInstance) {
   // Ensure HOST and JWT_SECRET is set
-  if (!process.env.HOST) throw new Error('Environment variable HOST is required');
+  if (!process.env.BASE_URL) throw new Error('Environment variable BASE_URL is required');
   if (!process.env.JWT_SECRET) throw new Error('Environment variable JWT_SECRET is required');
 
   // Register JWT setup plugin
