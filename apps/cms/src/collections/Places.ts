62a3404c79a8771c838b98a58f0c01c67f775297
Ole Kristian Losvik <losvik@gmail.com>
2024-04-06 14:01:32 +0200

    feat(cms): adds places collection

---
+++ apps/cms/src/collections/Places.ts
@@ -0,0 +1,55 @@
+import { CollectionConfig } from 'payload/types';
+import { randomUUID } from 'crypto';
+
+const Place: CollectionConfig = {
+  slug: 'places',
+  access: {
+    read: () => true,
+  },
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'type',
+      type: 'select',
+      options: [
+        { label: 'City', value: 'city' },
+        { label: 'Hotel', value: 'hotel' },
+        { label: 'House', value: 'house' },
+      ],
+      required: true,
+    },
+    {
+      name: 'description',
+      type: 'textarea',
+    },
+    {
+      name: 'postalAddress',
+      type: 'group',
+      fields: [
+        { name: 'streetAddress', type: 'text' },
+        { name: 'city', type: 'text' },
+        { name: 'region', type: 'text' },
+        { name: 'postalCode', type: 'text' },
+        { name: 'country', type: 'text' },
+      ],
+    },
+    {
+      name: 'parentPlace',
+      label: 'Contained In',
+      type: 'relationship',
+      relationTo: 'places',
+      admin: {
+        description: 'Select the place this one is contained in, if any.',
+      },
+    },
+  ],
+};
+
+export default Place;
--- apps/cms/src/payload.config.ts
+++ apps/cms/src/payload.config.ts
@@ -7,6 +7,7 @@ import { slateEditor } from '@payloadcms/richtext-slate'
 import { buildConfig } from 'payload/config'
 
 import Users from './collections/Users'
+import Places from './collections/Places';
 
 export default buildConfig({
   admin: {
@@ -14,7 +15,7 @@ export default buildConfig({
     bundler: webpackBundler(),
   },
   editor: slateEditor({}),
-  collections: [Users],
+  collections: [Places, Users],
   serverURL: process.env.PAYLOAD_SERVER_URL || 'http://localhost:3200',
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
@@ -24,6 +25,7 @@ export default buildConfig({
   },
   plugins: [payloadCloud()],
   db: postgresAdapter({
+    idType: "uuid",
     pool: {
       connectionString: process.env.PAYLOAD_DB_URL,
     },
