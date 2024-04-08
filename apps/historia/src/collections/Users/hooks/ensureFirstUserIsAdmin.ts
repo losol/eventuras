11e919c6c62d57eab564ba1c34d0a991b2a683ff
Ole Kristian Losvik <losvik@gmail.com>
2024-04-08 21:24:28 +0200

    feat(historia): add roles to users model

---
+++ apps/historia/src/collections/Users/hooks/ensureFirstUserIsAdmin.ts
@@ -0,0 +1,17 @@
+import type { FieldHook } from 'payload/types'
+
+import type { User } from '../../../payload-types'
+
+export const ensureFirstUserIsAdmin: FieldHook<User> = async ({ req, operation, value }) => {
+  if (operation === 'create') {
+    const users = await req.payload.find({ collection: 'users', limit: 0, depth: 0 })
+    if (users.totalDocs === 0) {
+      // if `admin` not in array of values, add it
+      if (!(value || []).includes('admin')) {
+        return [...(value || []), 'admin']
+      }
+    }
+  }
+
+  return value
+}
--- apps/historia/src/payload.config.ts
+++ apps/historia/src/payload.config.ts
@@ -1,14 +1,13 @@
-import path from 'path'
-
-import { payloadCloud } from '@payloadcms/plugin-cloud'
-import { postgresAdapter } from '@payloadcms/db-postgres'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { payloadCloud } from '@payloadcms/plugin-cloud'
 import { slateEditor } from '@payloadcms/richtext-slate'
+import path from 'path'
 import { buildConfig } from 'payload/config'
 
-import Users from './collections/Users'
-import Places from './collections/Places';
 import Happenings from './collections/Happenings';
+import Places from './collections/Places';
+import Users from './collections/Users'
 
 export default buildConfig({
   admin: {
