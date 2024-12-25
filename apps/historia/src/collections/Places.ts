45ae66a2c7ecec046c7e928e3b035a36ce644262
Ole Kristian Losvik <losvik@gmail.com>
2024-12-25 23:36:30 +0100

    feat: add geopoint to places

--- apps/historia/src/collections/Places.ts
+++ apps/historia/src/collections/Places.ts
@@ -2,6 +2,7 @@ import type { CollectionConfig } from 'payload';
 import { admins } from '../access/admins';
 import { anyone } from '../access/anyone';
 import { story } from '../fields/story';
+import { geoPoint } from '@/fields/geopoint';
 
 export const Places: CollectionConfig = {
   slug: 'places',
@@ -21,29 +22,43 @@ export const Places: CollectionConfig = {
       required: true,
     },
     {
-      name: 'type',
-      type: 'select',
-      options: [
-        { label: 'City', value: 'city' },
-        { label: 'Hotel', value: 'hotel' },
-        { label: 'House', value: 'house' },
-      ],
-      required: true,
-    },
-    {
       name: 'description',
       type: 'textarea',
     },
     {
-      name: 'postalAddress',
-      type: 'group',
-      fields: [
-        { name: 'streetAddress', type: 'text' },
-        { name: 'city', type: 'text' },
-        { name: 'region', type: 'text' },
-        { name: 'postalCode', type: 'text' },
-        { name: 'country', type: 'text' },
+      name: 'type',
+      type: 'select',
+      options: [
+        { label: 'Building', value: 'building' },
+        { label: 'Business', value: 'business' },
+        { label: 'City', value: 'city' },
+        { label: 'Hotel', value: 'hotel' },
+        { label: 'Residence', value: 'residence' },
+        { label: 'Other', value: 'other' }
       ],
+      defaultValue: 'other',
+      required: true,
+    },
+    {
+      label: 'Postal Address',
+      type: 'collapsible',
+      fields: [{
+        name: 'postalAddress',
+        type: 'group',
+        fields: [
+          { name: 'streetAddress', type: 'text' },
+          { name: 'region', type: 'text' },
+          { name: 'postalCode', type: 'text' },
+          { name: 'city', type: 'text' },
+          { name: 'country', type: 'text' },
+        ],
+      },
+      ]
+    },
+    {
+      label: 'Geo point',
+      type: 'collapsible',
+      fields: [geoPoint]
     },
     story
   ],
