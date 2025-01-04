6959258bb3aea71126b96e9a13ac5fab7c078a6f
Ole Kristian Losvik <losvik@gmail.com>
2025-01-04 21:40:43 +0100

    fix(historia): page refresh

--- apps/historia/src/Header/Component.client.tsx
+++ apps/historia/src/Header/Component.client.tsx
@@ -6,30 +6,32 @@ import React, { useEffect, useState } from 'react'
 
 import { Logo } from '@/components/Logo/Logo'
 
-interface HeaderClientProps {
-}
+interface HeaderClientProps {}
 
 export const HeaderClient: React.FC<HeaderClientProps> = () => {
-  /* Storing the value in a useState to avoid hydration errors */
-  const [theme, setTheme] = useState<string | null>(null)
-  const { headerTheme, setHeaderTheme } = useHeaderTheme()
-  const pathname = usePathname()
+  const [theme, setTheme] = useState<string | null>(null);
+  const { headerTheme, setHeaderTheme } = useHeaderTheme();
+  const pathname = usePathname();
 
   useEffect(() => {
-    setHeaderTheme(null)
-  }, [pathname])
+    if (headerTheme !== null) {
+      setHeaderTheme(null);
+    }
+  }, [pathname, headerTheme]);
 
   useEffect(() => {
-    if (headerTheme && headerTheme !== theme) setTheme(headerTheme)
-  }, [headerTheme])
+    if (headerTheme && headerTheme !== theme) {
+      setTheme(headerTheme);
+    }
+  }, [headerTheme, theme]);
 
   return (
-    <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
+    <header className={`container relative z-20 ${theme ? `data-theme=${theme}` : ''}`}>
       <div className="py-8 flex justify-between">
         <Link href="/">
           <Logo className="invert dark:invert-0" />
         </Link>
       </div>
     </header>
-  )
-}
+  );
+};
