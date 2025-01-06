a8f63e5921e66c7c72e0c25fb572135461695f27
Ole Kristian Losvik <losvik@gmail.com>
2025-01-06 23:01:19 +0100

    refactor: deduplicate slug field

--- apps/historia/src/collections/Categories.ts
+++ apps/historia/src/collections/Categories.ts
@@ -2,7 +2,7 @@ import type { CollectionConfig } from 'payload';
 
 import { anyone } from '../access/anyone';
 import { authenticated } from '../access/authenticated';
-import { slugField } from '@/slug';
+import { slugField } from '@/fields/slug';
 
 export const Categories: CollectionConfig = {
   slug: 'topics',
