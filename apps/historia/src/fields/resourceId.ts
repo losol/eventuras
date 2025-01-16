2559f766690aa00f30ca7f4c7ea508269940a974
Ole Kristian Losvik <losvik@gmail.com>
2025-01-16 20:36:18 +0100

    feat(historia): redirect on wrong slug

--- apps/historia/src/fields/resourceId.ts
+++ apps/historia/src/fields/resourceId.ts
@@ -2,7 +2,7 @@ import { customAlphabet } from 'nanoid';
 import { Field } from 'payload';
 
 // Define the custom alphabet
-const generateId = customAlphabet('123456789abcdefghijkmnpqrstuvwxyz', 6);
+const generateId = customAlphabet('123456789abcdefghijkmnpqrstuvwxyz', 8);
 
 // Define the field
 const resourceId: Field = {
