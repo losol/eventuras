bf02b2333e8e27c29f5f686c17796cfd6118a4cb
Yura <yura.bartish@gmail.com>
2025-04-08 11:56:07 +0300

    feat(api): orders population by registrations

--- apps/api/src/Eventuras.Services/Orders/IOrderRetrievalService.cs
+++ apps/api/src/Eventuras.Services/Orders/IOrderRetrievalService.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Eventuras.Domain;
-using static Eventuras.Services.Orders.OrderRetrievalService;
 
 namespace Eventuras.Services.Orders;
 
@@ -17,9 +16,15 @@ public interface IOrderRetrievalService
         OrderRetrievalOptions options = default,
         CancellationToken cancellationToken = default);
 
-
     Task<ProductDeliverySummaryDto> GetProductDeliverySummaryAsync(
         int productId,
         CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Retrieves orders and populates them with data from registrations.
+    /// </summary>
+    /// <param name="orderIds">List of order IDs to retrieve and populate.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>List of orders populated with registration data.</returns>
+    Task<List<Order>> GetOrdersPopulatedByRegistrationAsync(IEnumerable<int> orderIds, CancellationToken cancellationToken = default);
 }
