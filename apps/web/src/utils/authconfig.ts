5efc650bc640739f3ccc4ebdfc46d0832db4713b
Ole Kristian Losvik <losvik@gmail.com>
2025-04-11 19:13:13 +0200

    fix: correct redirect uri

--- apps/web/src/utils/authconfig.ts
+++ apps/web/src/utils/authconfig.ts
@@ -4,7 +4,7 @@ import * as openid from 'openid-client';
 import Environment, { EnvironmentVariables } from '@/utils/Environment';
 
 export const auth0callbackUrl =
-  Environment.NEXT_PUBLIC_APPLICATION_URL + 'api/login/auth0/callback';
+  Environment.NEXT_PUBLIC_APPLICATION_URL + '/api/login/auth0/callback';
 
 export const authConfig: OAuthConfig = {
   issuer: 'https://' + Environment.NEXT_PUBLIC_AUTH0_DOMAIN,
