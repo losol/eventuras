5c26ff410fbf3cfcdb838496b6cc882db1cc9641
Ole Kristian Losvik <losvik@gmail.com>
2025-04-12 22:31:40 +0200

    fix(web): get auth state machine back

--- apps/web/src/app/Providers.tsx
+++ apps/web/src/app/Providers.tsx
@@ -1,17 +1,27 @@
 'use client';
+import { useMemo } from 'react';
 import { RecoilRoot } from 'recoil';
+import { createActor } from 'xstate';
 
 import NotificationsProvider from '@/components/NotificationsProvider';
+import AuthenticationFlowMachine, {
+  AuthenticationStateContext,
+} from '@/statemachines/AuthenticationFlowMachine';
 
 type ProvidersProps = {
   children: React.ReactNode;
 };
 
 export default function Providers({ children }: ProvidersProps) {
+  const auth = useMemo(() => createActor(AuthenticationFlowMachine), []);
+  auth.start();
+
   return (
     <RecoilRoot>
       <NotificationsProvider />
-      {children}
+      <AuthenticationStateContext.Provider value={{ auth }}>
+        {children}
+      </AuthenticationStateContext.Provider>
     </RecoilRoot>
   );
 }
