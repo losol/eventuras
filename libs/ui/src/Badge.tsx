ca76cd14dc386eebcb09cd2728bd729be8dfe628
Ole Kristian Losvik <losvik@gmail.com>
2025-04-19 13:06:48 +0200

    fix: some tailwind css adjustments

--- libs/ui/src/Badge.tsx
+++ libs/ui/src/Badge.tsx
@@ -13,22 +13,28 @@ const Badge: React.FC<BadgeProps> = ({
   variant = 'neutral',
   block = false,
 }) => {
-  const variantClasses = {
-    neutral: 'bg-gray-500 dark:bg-gray-700',
-    info: 'bg-blue-500 dark:bg-blue-700',
-    positive: 'bg-green-500 dark:bg-green-700',
-    negative: 'bg-red-500 dark:bg-red-700',
-  };
+  let variantClass = '';
+
+  if (variant === 'neutral') {
+    variantClass = 'bg-gray-500 dark:bg-gray-700';
+  } else if (variant === 'info') {
+    variantClass = 'bg-blue-500 dark:bg-blue-700';
+  } else if (variant === 'positive') {
+    variantClass = 'bg-green-500 dark:bg-green-700';
+  } else if (variant === 'negative') {
+    variantClass = 'bg-red-500 dark:bg-red-700';
+  }
 
   const blockClass = block ? 'block py-2 my-2' : '';
 
-  return (
-    <span
-      className={`${blockClass} px-2 py-1 text-xs font-semibold leading-none text-white rounded-xs ${variantClasses[variant]} ${className}`}
-    >
-      {children}
-    </span>
-  );
+  const allClasses = `
+    ${blockClass}
+    ${variantClass}
+    px-2 py-1 text-xs font-semibold leading-none text-white rounded-xs
+    ${className}
+  `.trim();
+
+  return <span className={allClasses}>{children}</span>;
 };
 
 export default Badge;
