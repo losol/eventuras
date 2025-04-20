f9cff12a443ff72d5b982868b25ad84d4b79fa0d
Ole Kristian Losvik <losvik@gmail.com>
2025-04-20 20:37:23 +0200

    feat(ui): add storybook themes

--- libs/ratio-ui/.storybook/preview.ts
+++ libs/ratio-ui/.storybook/preview.ts
@@ -1,16 +1,19 @@
 import type { Preview } from '@storybook/react';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
 
 import "../src/ratio-ui.css";
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+  decorators: [
+    withThemeByDataAttribute({
+      themes: {
+        light: 'light',
+        dark: 'dark',
       },
-    },
-  },
+      defaultTheme: 'light',
+      attributeName: 'data-theme',
+    }),
+  ],
 };
 
 export default preview;
