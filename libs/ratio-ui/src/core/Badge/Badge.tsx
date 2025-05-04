f351bee2f4380501563a24c42cb13caa8a486bd7
Ole Kristian Losvik <losvik@gmail.com>
2025-05-05 00:00:58 +0200

    feat(ratio): adds some more stories

--- libs/ratio-ui/src/core/Badge/Badge.tsx
+++ libs/ratio-ui/src/core/Badge/Badge.tsx
@@ -16,21 +16,21 @@ const Badge: React.FC<BadgeProps> = ({
   let variantClass = '';
 
   if (variant === 'neutral') {
-    variantClass = 'bg-gray-500 dark:bg-gray-700';
+    variantClass = 'bg-gray-700 dark:bg-gray-800';
   } else if (variant === 'info') {
-    variantClass = 'bg-blue-500 dark:bg-blue-700';
+    variantClass = 'bg-blue-600 dark:bg-blue-800';
   } else if (variant === 'positive') {
-    variantClass = 'bg-green-500 dark:bg-green-700';
+    variantClass = 'bg-green-700 dark:bg-green-800';
   } else if (variant === 'negative') {
-    variantClass = 'bg-red-500 dark:bg-red-700';
+    variantClass = 'bg-red-600 dark:bg-red-800 text-white';
   }
 
-  const blockClass = block ? 'block py-2 my-2' : '';
+  const blockClass = block ? 'block' : '';
 
   const allClasses = `
     ${blockClass}
     ${variantClass}
-    px-2 py-1 text-xs font-semibold leading-none text-white rounded-xs
+    p-2 text-xs leading-none text-white rounded
     ${className}
   `.trim();
 
