d6824b933a2053f9ed196f7469461cdff256af37
Ole Kristian Losvik <losvik@gmail.com>
2025-06-16 23:11:49 +0200

    chore(event-sdk): upgrade hey api client

--- libs/event-sdk/src/clientConfig.ts
+++ libs/event-sdk/src/clientConfig.ts
@@ -1,16 +1,6 @@
-import {
-  createClient as heyCreateClient,
-  createConfig as heyCreateConfig,
-} from '@hey-api/client-next';
-
-import type { ClientOptions } from './client/types.gen';
 import type { CreateClientConfig } from './client/client.gen';
-
 
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
   baseUrl: 'http://localhost:8080',
 });
-
-export const createClient = heyCreateClient;
-export const createConfig = heyCreateConfig<ClientOptions>;
