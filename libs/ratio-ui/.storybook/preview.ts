fdef701683ca1c97c37c4ba45e0c7c64edcd306a
Ole Kristian Losvik <losvik@gmail.com>
2025-06-16 22:13:15 +0200

    chore(ratio-ui): upgrade to storybook v9

--- libs/ratio-ui/.storybook/preview.ts
+++ libs/ratio-ui/.storybook/preview.ts
@@ -1,4 +1,4 @@
-import type { Preview } from '@storybook/react';
+import type { Preview } from '@storybook/react-vite';
 import { withThemeByDataAttribute } from '@storybook/addon-themes';
 import ratioTheme from "./theme";
 
