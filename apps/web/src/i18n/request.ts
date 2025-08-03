4d8ef4fa0bef6b82d2be59548f0ffb959d9450c7
Ole Kristian Losvik <losvik@gmail.com>
2025-08-04 00:41:26 +0200

    fix(web): reads locale from env again

--- apps/web/src/i18n/request.ts
+++ apps/web/src/i18n/request.ts
@@ -1,9 +1,10 @@
+import Environment from '@/utils/Environment';
 import { getRequestConfig } from 'next-intl/server';
 
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
-  const locale = 'en-US';
+  const locale = Environment.NEXT_PUBLIC_DEFAULT_LOCALE || 'en-US';
 
   const messages = {
     admin: { ...(await import(`../../locales/${locale}/admin.json`)).default },
