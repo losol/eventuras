b47c4f2796c8d6f4b8f58644ebdd4c44515faea3
Ole Kristian Losvik <losvik@gmail.com>
2025-10-18 21:48:06 +0200

    chore(web): remove pino-pretty support

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,43 +3,12 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: [
-    '@eventuras/sdk',
-    '@eventuras/scribo',
-    '@eventuras/ratio-ui',
-    '@eventuras/fides-auth',
-    '@eventuras/smartform',
-    '@eventuras/datatable',
-    '@eventuras/logger',
-    '@eventuras/toast',
-    '@eventuras/markdowninput',
-    '@eventuras/event-sdk',
-  ],
+  transpilePackages: ['@eventuras/sdk', '@eventuras/scribo'],
 
   reactStrictMode: true,
 
   images: {
     unoptimized: true,
-  },
-
-  // Exclude pino from Next.js bundling - load as external packages
-  serverExternalPackages: [
-    'pino',
-    'pino-pretty',
-    'pino-abstract-transport',
-  ],
-
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Ignore worker_threads and other node-specific modules in server bundles
-      config.externals = config.externals || [];
-      config.externals.push({
-        'worker_threads': 'commonjs worker_threads',
-        'pino-pretty': 'commonjs pino-pretty',
-        'pino-abstract-transport': 'commonjs pino-abstract-transport',
-      });
-    }
-    return config;
   },
 };
 
