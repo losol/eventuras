aff4a8c6b4801c86efd0d75de797d268d04685d8
Ole Kristian Losvik <losvik@gmail.com>
2025-10-19 03:06:57 +0200

    chore(smartform): mark deps as external

--- libs/smartform/src/Form.tsx
+++ libs/smartform/src/Form.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { FC, ReactNode } from 'react';
-import { FormProvider, useForm, UseFormProps } from 'react-hook-form';
+import { type FC, type ReactNode } from 'react';
+import { FormProvider, useForm, type UseFormProps } from 'react-hook-form';
 
 interface FormProps extends UseFormProps {
   children: ReactNode;
@@ -12,17 +12,17 @@ interface FormProps extends UseFormProps {
 
 const defaultFormClassName = 'px-8 pt-6 pb-8 mb-4';
 
-const Form: FC<FormProps> = (props) => {
-  const methods = useForm({ defaultValues: props.defaultValues });
+const Form: FC<FormProps> = ({ children, onSubmit, className, testId, ...formOptions }) => {
+  const methods = useForm(formOptions);
 
   return (
     <FormProvider {...methods}>
       <form
-        onSubmit={methods.handleSubmit(props.onSubmit)}
-        className={props.className ?? defaultFormClassName}
-        data-testid={props.testId}
+        onSubmit={methods.handleSubmit(onSubmit)}
+        className={className ?? defaultFormClassName}
+        data-testid={testId}
       >
-        {props.children}
+        {children}
       </form>
     </FormProvider>
   );
--- libs/smartform/vite.config.ts
+++ libs/smartform/vite.config.ts
@@ -2,4 +2,9 @@ import { defineReactLibConfig } from '@eventuras/vite-config/react-lib';
 
 export default defineReactLibConfig({
   entry: 'src/index.ts',
+  external: [
+    'react-hook-form',
+    'react-aria-components',
+    '@tabler/icons-react',
+  ],
 });
