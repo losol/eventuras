6293d1b52a6e90259105a0d0b78808b37d2c6c31
Ole Kristian Losvik <losvik@gmail.com>
2025-10-28 20:32:58 +0100

    feat(web): enhance AdminPage layout with ButtonGroup and improved spacing

--- libs/ratio-ui/src/core/Button/ButtonGroup.tsx
+++ libs/ratio-ui/src/core/Button/ButtonGroup.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
 
-import { BoxProps, BoxSpacingProps, buildSpacingClasses } from '../../layout/Box/Box';
+import {
+  BoxProps,
+  BoxSpacingProps,
+  BoxContentProps,
+  BoxBackgroundProps,
+  buildSpacingClasses,
+} from '../../layout/Box/Box';
 
-export interface ButtonGroupProps extends BoxSpacingProps {
+export interface ButtonGroupProps {
   children: React.ReactNode;
+  /**
+   * Whether buttons should wrap to multiple lines when space is limited
+   * @default false
+   */
+  wrap?: boolean;
 }
 
-const ButtonGroup: React.FC<ButtonGroupProps & BoxProps> = ({ children, ...spacingProps }) => {
-  const baseClassName = 'inline-flex';
-  const className = [baseClassName, buildSpacingClasses(spacingProps)].join(' ');
+const ButtonGroup: React.FC<
+  ButtonGroupProps & BoxSpacingProps & BoxContentProps & BoxBackgroundProps
+> = ({ children, wrap = false, gap = '2', className: additionalClassName, ...spacingProps }) => {
+  const baseClassName = wrap ? 'flex' : 'inline-flex';
+  const wrapClassName = wrap ? 'flex-wrap' : '';
+  const className = [
+    baseClassName,
+    wrapClassName,
+    buildSpacingClasses({ ...spacingProps, gap }),
+    additionalClassName,
+  ]
+    .filter(Boolean)
+    .join(' ');
 
   return (
     <div className={className} role="group">
