d1eec7a62146a32bad2b9727cb5a36cdeb4c8aa1
Ole Kristian Losvik <losvik@gmail.com>
2025-12-09 19:07:41 +0100

    fix(scribo): import MarkdownEditor.css for styling

--- libs/scribo/src/index.tsx
+++ libs/scribo/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import './App.css'
+import './MarkdownEditor.css'
 
 import readmeRaw from '../README.md?raw'
 
