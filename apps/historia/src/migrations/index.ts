ba6277b4964606cb49ff21aaa0b60baee13f2696
Ole Kristian Losvik <losvik@gmail.com>
2025-12-18 00:38:58 +0100

    fix(historia): add the missing migrations

--- apps/historia/src/migrations/index.ts
+++ apps/historia/src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251213_223630_initial from './20251213_223630_initial';
 import * as migration_20251216_202151_mcp from './20251216_202151_mcp';
+import * as migration_20251217_233706_contactinfos from './20251217_233706_contactinfos';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20251216_202151_mcp.up,
     down: migration_20251216_202151_mcp.down,
-    name: '20251216_202151_mcp'
+    name: '20251216_202151_mcp',
+  },
+  {
+    up: migration_20251217_233706_contactinfos.up,
+    down: migration_20251217_233706_contactinfos.down,
+    name: '20251217_233706_contactinfos'
   },
 ];
