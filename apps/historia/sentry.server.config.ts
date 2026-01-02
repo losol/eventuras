c42c42c86401bce3111d6d9d06d2a76684bde926
Ole Kristian Losvik <losvik@gmail.com>
2026-01-03 00:55:32 +0100

    feat(historia): integrate Sentry for error tracking
    - Added Sentry configuration for edge features in `sentry.edge.config.ts`
    - Added Sentry configuration for server requests in `sentry.server.config.ts`
    - Implemented global error handling with Sentry in `global-error.tsx`
    - Configured client-side Sentry initialization in `instrumentation-client.ts`
    - Registered Sentry initialization based on runtime in `instrumentation.ts`

---
+++ apps/historia/sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_CMS_SENTRY_DSN,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
---
+++ apps/historia/src/instrumentation-client.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.NEXT_PUBLIC_CMS_SENTRY_DSN,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
