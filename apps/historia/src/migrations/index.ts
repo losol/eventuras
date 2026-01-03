ed4bf13ba51da1fe160c61c15d71cacca17f326d
Ole Kristian Losvik <losvik@gmail.com>
2026-01-03 20:30:24 +0100

    fix(historia): allow null values for order_id in transactions table

--- apps/historia/src/migrations/index.ts
+++ apps/historia/src/migrations/index.ts
@@ -1,6 +1,7 @@
 import * as migration_20251213_223630_initial from './20251213_223630_initial';
 import * as migration_20251216_202151_mcp from './20251216_202151_mcp';
 import * as migration_20251217_233706_contactinfos from './20251217_233706_contactinfos';
+import * as migration_20260103_192818_nullables from './20260103_192818_nullables';
 
 export const migrations = [
   {
@@ -16,6 +17,11 @@ export const migrations = [
   {
     up: migration_20251217_233706_contactinfos.up,
     down: migration_20251217_233706_contactinfos.down,
-    name: '20251217_233706_contactinfos'
+    name: '20251217_233706_contactinfos',
+  },
+  {
+    up: migration_20260103_192818_nullables.up,
+    down: migration_20260103_192818_nullables.down,
+    name: '20260103_192818_nullables'
   },
 ];
