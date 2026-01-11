b0a09bdec5763bef5aa494505de889c7b880146b
Ole Kristian Losvik <losvik@gmail.com>
2026-01-11 18:18:56 +0100

    chore(historia): code review fixes

--- libs/notitia-templates/src/templates/utils/index.ts
+++ libs/notitia-templates/src/templates/utils/index.ts
@@ -1,4 +1,4 @@
 export { buildOrderStatusTemplate, ORDER_STATUS_STYLES } from './orderStatusBuilder';
-export type { OrderConfirmationTexts } from './orderConfirmationBuilder';
+export type { OrderStatusTexts } from './orderStatusBuilder';
 export { buildOrderShippedTemplate, ORDER_SHIPPED_STYLES } from './orderShippedBuilder';
 export type { OrderShippedTexts } from './orderShippedBuilder';
