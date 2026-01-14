00d2ea0d73146a952100ceaf34ac9cdb0abd2f86
Ole Kristian Losvik <losvik@gmail.com>
2026-01-14 23:13:56 +0100

    chore(ratio-ui): cleaning

--- libs/ratio-ui/.storybook/main.ts
+++ libs/ratio-ui/.storybook/main.ts
@@ -8,7 +8,6 @@ const config: StorybookConfig = {
   "addons": [
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
-    '@storybook/addon-onboarding',
     '@storybook/addon-themes',
     '@storybook/addon-vitest'
   ],
