1da30bdb37d64227c4453727d531fa48a3144982
Ole Kristian Losvik <losvik@gmail.com>
2026-03-07 21:39:10 +0100

    chore(converto): update imports to use node module syntax

--- apps/convertoapi/eslint.config.js
+++ apps/convertoapi/eslint.config.js
@@ -1,4 +1,1 @@
-import { config } from '@eventuras/eslint-config/base';
-
-/** @type {import("eslint").Linter.Config[]} */
-export default config;
+export { config as default } from '@eventuras/eslint-config/base';
