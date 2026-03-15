b489974bd2654c42544bcb229837d5ee1d3ec20f
Ole Kristian Losvik <losvik@gmail.com>
2026-03-15 23:12:44 +0100

    refactor(ratio-ui): update eslint configuration to use flat config format

--- libs/ratio-ui/eslint.config.js
+++ libs/ratio-ui/eslint.config.js
@@ -1,7 +1,5 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+import storybook from 'eslint-plugin-storybook';
+import { config as reactLibraryConfig } from '@eventuras/eslint-config/react-library';
 
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ['@eventuras/eslint-config/react-library.js'],
-};
+/** @type {import("eslint").Linter.Config[]} */
+export default [...reactLibraryConfig, ...storybook.configs['flat/recommended']];
