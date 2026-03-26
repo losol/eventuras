ce81ae5ef11f07b302533245ed4eb03f006b77a0
Ole Kristian Losvik <losvik@gmail.com>
2026-03-26 17:38:44 +0100

    refactor(web): simplify theme initialization script

--- apps/web/src/providers/theme/InitTheme.tsx
+++ apps/web/src/providers/theme/InitTheme.tsx
@@ -1,48 +1,33 @@
-import Script from 'next/script';
-
 import { defaultTheme, themeLocalStorageKey } from './shared';
 
+const themeScript = `
+(function () {
+  function getImplicitPreference() {
+    var mediaQuery = '(prefers-color-scheme: dark)'
+    var mql = window.matchMedia(mediaQuery)
+    var hasImplicitPreference = typeof mql.matches === 'boolean'
+    if (hasImplicitPreference) {
+      return mql.matches ? 'dark' : 'light'
+    }
+    return null
+  }
+  function themeIsValid(theme) {
+    return theme === 'light' || theme === 'dark'
+  }
+  var themeToSet = '${defaultTheme}'
+  var preference = window.localStorage.getItem('${themeLocalStorageKey}')
+  if (themeIsValid(preference)) {
+    themeToSet = preference
+  } else {
+    var implicitPreference = getImplicitPreference()
+    if (implicitPreference) {
+      themeToSet = implicitPreference
+    }
+  }
+  document.documentElement.setAttribute('data-theme', themeToSet)
+})();
+`;
+
 export const InitTheme = () => {
-  return (
-    <Script
-      dangerouslySetInnerHTML={{
-        __html: `
-  (function () {
-    function getImplicitPreference() {
-      var mediaQuery = '(prefers-color-scheme: dark)'
-      var mql = window.matchMedia(mediaQuery)
-      var hasImplicitPreference = typeof mql.matches === 'boolean'
-
-      if (hasImplicitPreference) {
-        return mql.matches ? 'dark' : 'light'
-      }
-
-      return null
-    }
-
-    function themeIsValid(theme) {
-      return theme === 'light' || theme === 'dark'
-    }
-
-    var themeToSet = '${defaultTheme}'
-    var preference = window.localStorage.getItem('${themeLocalStorageKey}')
-
-    if (themeIsValid(preference)) {
-      themeToSet = preference
-    } else {
-      var implicitPreference = getImplicitPreference()
-
-      if (implicitPreference) {
-        themeToSet = implicitPreference
-      }
-    }
-
-    document.documentElement.setAttribute('data-theme', themeToSet)
-  })();
-  `,
-      }}
-      id="theme-script"
-      strategy="beforeInteractive"
-    />
-  );
+  return <script id="theme-script" dangerouslySetInnerHTML={{ __html: themeScript }} />;
 };
