91dd9085db38213cb00ce06117136d498a8353b3
Ole Kristian Losvik <losvik@gmail.com>
2026-08-05 02:10:58 +0200

    chore: markdown lib has moved

--- libs/markdown/src/index.ts
+++
@@ -1,9 +0,0 @@
-export {MarkdownContent} from './MarkdownContent'
-export {normalizeMarkdown} from './normalizeMarkdown'
-export type {MarkdownContentProps, SanitizeSchemaExtension} from './MarkdownContent'
-export { mergeSanitizeSchemas } from './mergeSanitizeSchemas'
-
-export { remarkCallout } from './remarkCallout'
-export type { CalloutType } from './remarkCallout'
-export { calloutComponents } from './calloutComponents'
-export { calloutSanitizeSchema } from './calloutSanitizeSchema'
