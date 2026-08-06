4da06c88392fdc7f21b6cfeba19ceb98b4323948
Ole Kristian Losvik <losvik@gmail.com>
2026-08-06 22:18:54 +0200

    chore: consume origo foundation packages from npm

--- libs/core/src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Core utilities for Eventuras
- * @packageDocumentation
- *
- * Import utilities from specific submodules:
- * - @eventuras/core/regex
- * - @eventuras/core/datetime
- * - @eventuras/core/currency
- */
